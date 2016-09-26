3625f96dfb9ab079160818a5ced95f98cbd01814
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2016-09-26 23:41:19 +0200

    Added Edit menu

--- samples/Draw2D.Wpf/Views/MainView.xaml.cs
+++ samples/Draw2D.Wpf/Views/MainView.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Windows;
+using Draw2D.Models;
 using Draw2D.Models.Containers;
 using Draw2D.ViewModels.Containers;
 using Microsoft.Win32;
@@ -73,6 +75,70 @@ namespace Draw2D.Wpf.Views
             App.Current.Windows[0].Close();
         }
 
+        private void EditCut_Click(object sender, RoutedEventArgs e)
+        {
+            var vm = this.DataContext as ShapesContainerViewModel;
+            if (vm != null)
+            {
+                Cut(vm);
+                RendererView.InvalidateVisual();
+            }
+        }
+
+        private void EditCopy_Click(object sender, RoutedEventArgs e)
+        {
+            var vm = this.DataContext as ShapesContainerViewModel;
+            if (vm != null)
+            {
+                Copy(vm);
+            }
+        }
+
+        private void EditPaste_Click(object sender, RoutedEventArgs e)
+        {
+            var vm = this.DataContext as ShapesContainerViewModel;
+            if (vm != null)
+            {
+                Paste(vm);
+                RendererView.InvalidateVisual();
+            }
+        }
+
+        private void EditDelete_Click(object sender, RoutedEventArgs e)
+        {
+            var vm = this.DataContext as ShapesContainerViewModel;
+            if (vm != null)
+            {
+                Delete(vm);
+                RendererView.InvalidateVisual();
+            }
+        }
+
+        private static string ToYaml(object graph)
+        {
+            using (var writer = new StringWriter())
+            {
+                var builder = new SerializerBuilder()
+                    .EnsureRoundtrip()
+                    .WithNamingConvention(new NullNamingConvention());
+                var serializer = builder.Build();
+                serializer.Serialize(writer, graph);
+                return writer.ToString();
+            }
+        }
+
+        private static T FromYaml<T>(string yaml)
+        {
+            using (var reader = new StringReader(yaml))
+            {
+                var builder = new DeserializerBuilder()
+                    .IgnoreUnmatchedProperties()
+                    .WithNamingConvention(new NullNamingConvention());
+                var deserializer = builder.Build();
+                return deserializer.Deserialize<T>(reader);
+            }
+        }
+
         private void New(ShapesContainerViewModel vm)
         {
             vm.CurrentTool.Clean(vm);
@@ -90,33 +156,67 @@ namespace Draw2D.Wpf.Views
         private void Open(string path, ShapesContainerViewModel vm)
         {
             var yaml = File.ReadAllText(path);
-            using (var reader = new StringReader(yaml))
-            {
-                var builder = new DeserializerBuilder()
-                    .IgnoreUnmatchedProperties()
-                    .WithNamingConvention(new NullNamingConvention());
-                var deserializer = builder.Build();
-                var container = deserializer.Deserialize<ShapesContainer>(reader);
-                var workingContainer = new ShapesContainer();
-                vm.CurrentTool.Clean(vm);
-                vm.Renderer.Selected.Clear();
-                vm.Container = container;
-                vm.WorkingContainer = workingContainer;
-            }
+            var container = FromYaml<ShapesContainer>(yaml);
+            var workingContainer = new ShapesContainer();
+            vm.CurrentTool.Clean(vm);
+            vm.Renderer.Selected.Clear();
+            vm.Container = container;
+            vm.WorkingContainer = workingContainer;
         }
 
         private void Save(string path, ShapesContainerViewModel vm)
         {
-            using (var writer = new StringWriter())
+            var yaml = ToYaml(vm.Container);
+            File.WriteAllText(path, yaml);
+        }
+
+        private void Cut(ShapesContainerViewModel vm)
+        {
+            Copy(vm);
+            Delete(vm);
+        }
+
+        private void Copy(ShapesContainerViewModel vm)
+        {
+            var selected = vm.Renderer.Selected;
+            var shapes = new List<BaseShape>();
+            foreach (var shape in selected)
             {
-                var builder = new SerializerBuilder()
-                    .EmitDefaults()
-                    .EnsureRoundtrip()
-                    .WithNamingConvention(new NullNamingConvention());
-                var serializer = builder.Build();
-                serializer.Serialize(writer, vm.Container);
-                File.WriteAllText(path, writer.ToString());
+                if (vm.Container.Shapes.Contains(shape))
+                {
+                    shapes.Add(shape);
+                }
             }
+            var yaml = ToYaml(shapes);
+            Clipboard.SetText(yaml);
+        }
+
+        private void Paste(ShapesContainerViewModel vm)
+        {
+            if (Clipboard.ContainsText())
+            {
+                var yaml = Clipboard.GetText();
+                var shapes = FromYaml<List<BaseShape>>(yaml);
+                vm.Renderer.Selected.Clear();
+                foreach (var shape in shapes)
+                {
+                    vm.Container.Shapes.Add(shape);
+                    shape.Select(vm.Renderer.Selected);
+                }
+            }
+        }
+
+        private void Delete(ShapesContainerViewModel vm)
+        {
+            var selected = vm.Renderer.Selected;
+            foreach (var shape in selected)
+            {
+                if (vm.Container.Shapes.Contains(shape))
+                {
+                    vm.Container.Shapes.Remove(shape);
+                }
+            }
+            vm.Renderer.Selected.Clear();
         }
     }
 }
