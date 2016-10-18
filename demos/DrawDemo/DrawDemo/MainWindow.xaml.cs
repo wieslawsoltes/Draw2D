7b2c00470ecf38005479d93575e1a4c78f225dd1
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2016-10-18 14:51:55 +0200

    Added DrawDemo

---
+++ demos/DrawDemo/DrawDemo/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace DrawDemo
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class DrawCanvas : Canvas
+    {
+        bool _draw = false;
+        Point _point;
+        
+        protected override void OnMouseEnter(MouseEventArgs e)
+        {
+            base.OnMouseEnter(e);
+            //Debug.WriteLine("Enter");
+            _point = e.GetPosition(this);
+            _draw = true;
+            //Cursor = Cursors.None;
+            this.InvalidateVisual();
+        }
+
+        protected override void OnMouseLeave(MouseEventArgs e)
+        {
+            base.OnMouseLeave(e);
+            //Debug.WriteLine("Leave");
+            _draw = false;
+            //Cursor = Cursors.Arrow;
+            this.InvalidateVisual();
+        }
+
+        protected override void OnPreviewMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnPreviewMouseLeftButtonDown(e);
+        }
+        
+        protected override void OnPreviewMouseMove(MouseEventArgs e)
+        {
+            base.OnPreviewMouseMove(e);
+            //Debug.WriteLine("Move");
+            _point = e.GetPosition(this);
+            this.InvalidateVisual();
+        }
+        
+        protected override void OnRender(DrawingContext dc)
+        {
+            base.OnRender(dc);
+
+            if (_draw)
+            {
+                //Debug.WriteLine("Draw");
+                var pen = new Pen(Brushes.Cyan, 2);
+                
+                var a0 = new Point(0, _point.Y);
+                var b0 = new Point(this.ActualWidth, _point.Y);
+                var a1 = new Point(_point.X, 0);
+                var b1 = new Point(_point.X, this.ActualHeight);
+                
+                dc.DrawLine(pen, a0, b0);
+                dc.DrawLine(pen, a1, b1);
+                
+                //dc.DrawEllipse(Brushes.Yellow, null, _point, 5, 5);
+                
+            }
+        }
+    }
+}
---
+++ demos/DrawDemo/DrawDemo/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Resources;
+using System.Windows;
+
+[assembly: AssemblyTitle("DrawDemo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DrawDemo")]
+[assembly: AssemblyCopyright("Copyright 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
