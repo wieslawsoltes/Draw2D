b3532f33c5893150cb7b848392cb60aea4be3589
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2017-03-06 21:19:37 +0100

    Renamed class

---
+++ src/PathDemo/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace PathDemo
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
