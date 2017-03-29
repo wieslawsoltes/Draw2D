76c84fa9bbe9342274453fe18b7790191959aceb
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2017-03-29 11:52:59 +0200

    Update App.xaml.cs
    [skip ci]

--- src/PathDemo/App.xaml.cs
+++ src/PathDemo/App.xaml.cs
@@ -1,8 +1,16 @@
 ﻿using System.Windows;
+using PathDemo.Views;
 
 namespace PathDemo
 {
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var window = new MainView();
+            window.ShowDialog();
+        }
     }
 }
