0ca6890f8120e48c05b79c27bc6bd602bda5d006
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2017-03-29 14:24:03 +0200

    Update PathCanvas.cs

--- src/PathDemo/Controls/PathCanvas.cs
+++ src/PathDemo/Controls/PathCanvas.cs
@@ -11,116 +11,19 @@ using PathDemo.Tools;
 
 namespace PathDemo.Controls
 {
-    public class PathCanvas : Canvas, IToolContext
+    public class LineHelper
     {
-        public ISet<ShapeObject> Selected { get; set; }
-        public ObservableCollection<ShapeObject> Shapes { get; set; }
-
-        public ObservableCollection<ToolBase> Tools { get; set; }
-        public ToolBase CurrentTool { get; set; }
-
-        public PathCanvas()
-        {
-            Shapes = new ObservableCollection<ShapeObject>();
-            Selected = new HashSet<ShapeObject>();
-
-            Tools = new ObservableCollection<ToolBase>();
-            Tools.Add(new LineTool() { Name = "Line" });
-            Tools.Add(new CubicBezierTool() { Name = "CubicBezier" });
-            Tools.Add(new QuadraticBezierTool() { Name = "QuadraticBezier" });
-            Tools.Add(new PathTool() { Name = "Path" });
-            CurrentTool = Tools[0];
-        }
-
-        public PointShape GetNextPoint(Point point)
-        {
-            return new PointShape() { X = point.X, Y = point.Y };
-        }
-
-        public void Capture()
-        {
-            this.CaptureMouse();
-        }
-
-        public void Release()
-        {
-            this.ReleaseMouseCapture();
-        }
-
-        public void Invalidate()
-        {
-            this.InvalidateVisual();
-        }
-
-        protected override void OnPreviewMouseLeftButtonDown(MouseButtonEventArgs e)
-        {
-            base.OnPreviewMouseLeftButtonDown(e);
-
-            CurrentTool.LeftDown(this, e.GetPosition(this));
-        }
-
-        protected override void OnPreviewMouseLeftButtonUp(MouseButtonEventArgs e)
-        {
-            base.OnPreviewMouseLeftButtonUp(e);
-
-            CurrentTool.LeftUp(this, e.GetPosition(this));
-        }
-
-        protected override void OnPreviewMouseRightButtonDown(MouseButtonEventArgs e)
-        {
-            base.OnPreviewMouseRightButtonDown(e);
-
-            CurrentTool.RightDown(this, e.GetPosition(this));
-        }
-
-        protected override void OnPreviewMouseRightButtonUp(MouseButtonEventArgs e)
-        {
-            base.OnPreviewMouseRightButtonUp(e);
-
-            CurrentTool.RightUp(this, e.GetPosition(this));
-        }
-
-        protected override void OnPreviewMouseMove(MouseEventArgs e)
-        {
-            base.OnPreviewMouseMove(e);
-
-            CurrentTool.Move(this, e.GetPosition(this));
-        }
-
-        private void DrawLine(DrawingContext dc, LineShape line)
-        {
-            var brushPen = Brushes.Yellow;
-            var penStroke = new Pen(brushPen, 2);
-            var geometry = new StreamGeometry();
-            using (var context = geometry.Open())
-            {
-                context.BeginFigure(line.StartPoint.AsPoint(), false, false);
-                context.LineTo(line.Point.AsPoint(), true, false);
-            }
-            dc.DrawGeometry(null, penStroke, geometry);
-        }
-
-        private void DrawLineHelpers(DrawingContext dc, LineShape line)
+        public static void Draw(DrawingContext dc, LineShape line)
         {
             var brushPoints = Brushes.Yellow;
             dc.DrawEllipse(brushPoints, null, line.StartPoint.AsPoint(), 4, 4);
             dc.DrawEllipse(brushPoints, null, line.Point.AsPoint(), 4, 4);
         }
+    }
 
-        private void DrawCubicBezier(DrawingContext dc, CubicBezierShape cubicBezier)
-        {
-            var brushPen = Brushes.Yellow;
-            var penStroke = new Pen(brushPen, 2);
-            var geometry = new StreamGeometry();
-            using (var context = geometry.Open())
-            {
-                context.BeginFigure(cubicBezier.StartPoint.AsPoint(), false, false);
-                context.BezierTo(cubicBezier.Point1.AsPoint(), cubicBezier.Point2.AsPoint(), cubicBezier.Point3.AsPoint(), true, false);
-            }
-            dc.DrawGeometry(null, penStroke, geometry);
-        }
-
-        private void DrawCubicBezierHelpers(DrawingContext dc, CubicBezierShape cubicBezier)
+    public class CubiceBezierHelper
+    {
+        public static void Draw(DrawingContext dc, CubicBezierShape cubicBezier)
         {
             var brushLines = Brushes.Cyan;
             var penLines = new Pen(brushLines, 2.0);
@@ -133,21 +36,11 @@ namespace PathDemo.Controls
             dc.DrawEllipse(brushPoints, null, cubicBezier.Point2.AsPoint(), 4, 4);
             dc.DrawEllipse(brushPoints, null, cubicBezier.Point3.AsPoint(), 4, 4);
         }
+    }
 
-        private void DrawQuadraticBezier(DrawingContext dc, QuadraticBezierShape quadraticBezier)
-        {
-            var brushPen = Brushes.Yellow;
-            var penStroke = new Pen(brushPen, 2);
-            var geometry = new StreamGeometry();
-            using (var context = geometry.Open())
-            {
-                context.BeginFigure(quadraticBezier.StartPoint.AsPoint(), false, false);
-                context.QuadraticBezierTo(quadraticBezier.Point1.AsPoint(), quadraticBezier.Point2.AsPoint(), true, false);
-            }
-            dc.DrawGeometry(null, penStroke, geometry);
-        }
-
-        private void DrawQuadraticBezierHelpers(DrawingContext dc, QuadraticBezierShape quadraticBezier)
+    public class QuadraticBezierHelper
+    {
+        public static void Draw(DrawingContext dc, QuadraticBezierShape quadraticBezier)
         {
             var brushLines = Brushes.Cyan;
             var penLines = new Pen(brushLines, 2.0);
@@ -158,8 +51,45 @@ namespace PathDemo.Controls
             dc.DrawEllipse(brushPoints, null, quadraticBezier.Point1.AsPoint(), 4, 4);
             dc.DrawEllipse(brushPoints, null, quadraticBezier.Point2.AsPoint(), 4, 4);
         }
+    }
 
-        private Geometry ToGeometry(PathShape path)
+    public class PathHelper
+    {
+        public static void Draw(DrawingContext dc, PathShape path)
+        {
+            foreach (var figure in path.Figures)
+            {
+                foreach (var segment in figure.Segments)
+                {
+                    if (segment is LineShape line)
+                    {
+                        if (Selected.Contains(line))
+                        {
+                            LineHelper.Draw(dc, line);
+                        }
+                    }
+                    else if (segment is CubicBezierShape cubicBezier)
+                    {
+                        if (Selected.Contains(cubicBezier))
+                        {
+                            CubiceBezierHelper.Draw(dc, cubicBezier);
+                        }
+                    }
+                    else if (segment is QuadraticBezierShape quadraticBezier)
+                    {
+                        if (Selected.Contains(quadraticBezier))
+                        {
+                            QuadraticBezierHelper.Draw(dc, quadraticBezier);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public class ShapeRenderer
+    {
+        public static Geometry ToGeometry(PathShape path)
         {
             var geometry = new StreamGeometry()
             {
@@ -173,9 +103,8 @@ namespace PathDemo.Controls
                     bool isFirstSegment = true;
                     foreach (var segment in figure.Segments)
                     {
-                        if (segment is LineShape)
+                        if (segment is LineShape line)
                         {
-                            var line = segment as LineShape;
                             if (isFirstSegment)
                             {
                                 context.BeginFigure(line.StartPoint.AsPoint(), figure.IsFilled, figure.IsClosed);
@@ -183,9 +112,8 @@ namespace PathDemo.Controls
                             }
                             context.LineTo(line.Point.AsPoint(), true, false);
                         }
-                        else if (segment is CubicBezierShape)
+                        else if (segment is CubicBezierShape cubicBezier)
                         {
-                            var cubicBezier = segment as CubicBezierShape;
                             if (isFirstSegment)
                             {
                                 context.BeginFigure(cubicBezier.StartPoint.AsPoint(), figure.IsFilled, figure.IsClosed);
@@ -193,9 +121,8 @@ namespace PathDemo.Controls
                             }
                             context.BezierTo(cubicBezier.Point1.AsPoint(), cubicBezier.Point2.AsPoint(), cubicBezier.Point3.AsPoint(), true, false);
                         }
-                        else if (segment is QuadraticBezierShape)
+                        else if (segment is QuadraticBezierShape quadraticBezier)
                         {
-                            var quadraticBezier = segment as QuadraticBezierShape;
                             if (isFirstSegment)
                             {
                                 context.BeginFigure(quadraticBezier.StartPoint.AsPoint(), figure.IsFilled, figure.IsClosed);
@@ -210,47 +137,111 @@ namespace PathDemo.Controls
             return geometry;
         }
 
-        private void DrawPath(DrawingContext dc, PathShape path)
+        public static void DrawLine(DrawingContext dc, LineShape line)
+        {
+            var brushPen = Brushes.Yellow;
+            var penStroke = new Pen(brushPen, 2);
+            var geometry = new StreamGeometry();
+            using (var context = geometry.Open())
+            {
+                context.BeginFigure(line.StartPoint.AsPoint(), false, false);
+                context.LineTo(line.Point.AsPoint(), true, false);
+            }
+            dc.DrawGeometry(null, penStroke, geometry);
+        }
+
+        public static void DrawCubicBezier(DrawingContext dc, CubicBezierShape cubicBezier)
+        {
+            var brushPen = Brushes.Yellow;
+            var penStroke = new Pen(brushPen, 2);
+            var geometry = new StreamGeometry();
+            using (var context = geometry.Open())
+            {
+                context.BeginFigure(cubicBezier.StartPoint.AsPoint(), false, false);
+                context.BezierTo(cubicBezier.Point1.AsPoint(), cubicBezier.Point2.AsPoint(), cubicBezier.Point3.AsPoint(), true, false);
+            }
+            dc.DrawGeometry(null, penStroke, geometry);
+        }
+
+        public static void DrawQuadraticBezier(DrawingContext dc, QuadraticBezierShape quadraticBezier)
+        {
+            var brushPen = Brushes.Yellow;
+            var penStroke = new Pen(brushPen, 2);
+            var geometry = new StreamGeometry();
+            using (var context = geometry.Open())
+            {
+                context.BeginFigure(quadraticBezier.StartPoint.AsPoint(), false, false);
+                context.QuadraticBezierTo(quadraticBezier.Point1.AsPoint(), quadraticBezier.Point2.AsPoint(), true, false);
+            }
+            dc.DrawGeometry(null, penStroke, geometry);
+        }
+
+        public static void DrawPath(DrawingContext dc, PathShape path)
         {
             var brushPen = Brushes.Yellow;
             var penStroke = new Pen(brushPen, 2);
             var geometry = ToGeometry(path);
             dc.DrawGeometry(null, penStroke, geometry);
         }
+    }
 
-        private void DrawPathHelpers(DrawingContext dc, PathShape path)
+    public class PathCanvas : Canvas, IToolContext
+    {
+        public ISet<ShapeObject> Selected { get; set; }
+        public ObservableCollection<ShapeObject> Shapes { get; set; }
+        public ObservableCollection<ToolBase> Tools { get; set; }
+        public ToolBase CurrentTool { get; set; }
+
+        public PathCanvas()
         {
-            foreach (var figure in path.Figures)
+            Shapes = new ObservableCollection<ShapeObject>();
+            Selected = new HashSet<ShapeObject>();
+            Tools = new ObservableCollection<ToolBase>
             {
-                foreach (var segment in figure.Segments)
-                {
-                    if (segment is LineShape)
-                    {
-                        var line = segment as LineShape;
-                        if (Selected.Contains(line))
-                        {
-                            DrawLineHelpers(dc, line);
-                        }
-                    }
-                    else if (segment is CubicBezierShape)
-                    {
-                        var cubicBezier = segment as CubicBezierShape;
-                        if (Selected.Contains(cubicBezier))
-                        {
-                            DrawCubicBezierHelpers(dc, cubicBezier);
-                        }
-                    }
-                    else if (segment is QuadraticBezierShape)
-                    {
-                        var quadraticBezier = segment as QuadraticBezierShape;
-                        DrawQuadraticBezier(dc, quadraticBezier);
-                        if (Selected.Contains(quadraticBezier))
-                        {
-                            DrawQuadraticBezierHelpers(dc, quadraticBezier);
-                        }
-                    }
-                }
-            }
+                new LineTool() { Name = "Line" },
+                new CubicBezierTool() { Name = "CubicBezier" },
+                new QuadraticBezierTool() { Name = "QuadraticBezier" },
+                new PathTool() { Name = "Path" }
+            };
+            CurrentTool = Tools[0];
+        }
+
+        public PointShape GetNextPoint(Point point) => new PointShape() { X = point.X, Y = point.Y };
+
+        public void Capture() => this.CaptureMouse();
+
+        public void Release() => this.ReleaseMouseCapture();
+
+        public void Invalidate() => this.InvalidateVisual();
+
+        protected override void OnPreviewMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnPreviewMouseLeftButtonDown(e);
+            CurrentTool.LeftDown(this, e.GetPosition(this));
+        }
+
+        protected override void OnPreviewMouseLeftButtonUp(MouseButtonEventArgs e)
+        {
+            base.OnPreviewMouseLeftButtonUp(e);
+            CurrentTool.LeftUp(this, e.GetPosition(this));
+        }
+
+        protected override void OnPreviewMouseRightButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnPreviewMouseRightButtonDown(e);
+            CurrentTool.RightDown(this, e.GetPosition(this));
+        }
+
+        protected override void OnPreviewMouseRightButtonUp(MouseButtonEventArgs e)
+        {
+            base.OnPreviewMouseRightButtonUp(e);
+            CurrentTool.RightUp(this, e.GetPosition(this));
+        }
+
+        protected override void OnPreviewMouseMove(MouseEventArgs e)
+        {
+            base.OnPreviewMouseMove(e);
+            CurrentTool.Move(this, e.GetPosition(this));
         }
 
         protected override void OnRender(DrawingContext dc)
@@ -259,40 +250,36 @@ namespace PathDemo.Controls
 
             foreach (var shape in Shapes)
             {
-                if (shape is LineShape)
+                if (shape is LineShape line)
                 {
-                    var line = shape as LineShape;
-                    DrawLine(dc, line);
+                    ShapeRenderer.DrawLine(dc, line);
                     if (Selected.Contains(line))
                     {
-                        DrawLineHelpers(dc, line);
+                        LineHelper.Draw(dc, line);
                     }
                 }
-                else if (shape is CubicBezierShape)
+                else if (shape is CubicBezierShape cubicBezier)
                 {
-                    var cubicBezier = shape as CubicBezierShape;
-                    DrawCubicBezier(dc, cubicBezier);
+                    ShapeRenderer.DrawCubicBezier(dc, cubicBezier);
                     if (Selected.Contains(cubicBezier))
                     {
-                        DrawCubicBezierHelpers(dc, cubicBezier);
+                        CubiceBezierHelper.Draw(dc, cubicBezier);
                     }
                 }
-                else if (shape is QuadraticBezierShape)
+                else if (shape is QuadraticBezierShape quadraticBezier)
                 {
-                    var quadraticBezier = shape as QuadraticBezierShape;
-                    DrawQuadraticBezier(dc, quadraticBezier);
+                    ShapeRenderer.DrawQuadraticBezier(dc, quadraticBezier);
                     if (Selected.Contains(quadraticBezier))
                     {
-                        DrawQuadraticBezierHelpers(dc, quadraticBezier);
+                        QuadraticBezierHelper.Draw(dc, quadraticBezier);
                     }
                 }
-                else if (shape is PathShape)
+                else if (shape is PathShape path)
                 {
-                    var path = shape as PathShape;
-                    DrawPath(dc, path);
+                    ShapeRenderer.DrawPath(dc, path);
                     if (Selected.Contains(path))
                     {
-                        DrawPathHelpers(dc, path);
+                        PathHelper.Draw(dc, path);
                     }
                 }
             }
