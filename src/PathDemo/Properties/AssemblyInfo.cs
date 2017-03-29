c4c5f7ac50d5425671ec17482afd0c1873c82524
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2017-03-29 11:47:50 +0200

    Update AssemblyInfo.cs
    [skip ci]

--- src/PathDemo/Properties/AssemblyInfo.cs
+++ src/PathDemo/Properties/AssemblyInfo.cs
@@ -1,15 +1,18 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Windows;
 
 [assembly: AssemblyTitle("PathDemo")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A 2D vector diagram editor.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Wiesław Šoltés")]
 [assembly: AssemblyProduct("PathDemo")]
-[assembly: AssemblyCopyright("Copyright 2016")]
+[assembly: AssemblyCopyright("Copyright © Wiesław Šoltés 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
