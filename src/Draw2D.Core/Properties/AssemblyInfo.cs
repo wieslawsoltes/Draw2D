f39c68c6a9c3dc5c21674fed8ba0c9d1d7e5cfc4
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2017-03-30 18:44:32 +0200

    Changed project type

---
+++ src/Draw2D.Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Draw2D.Core")]
+[assembly: AssemblyDescription("A 2D vector diagram editor.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Wiesław Šoltés")]
+[assembly: AssemblyProduct("Draw2D.Core")]
+[assembly: AssemblyCopyright("Copyright © Wiesław Šoltés 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
