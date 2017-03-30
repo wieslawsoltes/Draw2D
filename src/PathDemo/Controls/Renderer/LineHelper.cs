337a333cdadf2e7f0a26a2ac7e529857e0287ec5
wieslawsoltes <wieslaw.soltes@gmail.com>
2017-03-30 13:37:32 +0000

    Moved common helper to separate file

--- src/PathDemo/Controls/Renderer/LineHelper.cs
+++ src/PathDemo/Controls/Renderer/LineHelper.cs
@@ -5,23 +5,6 @@ using Draw2D.Models.Style;
 
 namespace PathDemo.Controls
 {
-    public class CommonHelper
-    {
-        public static EllipseShape Ellipse = new EllipseShape(new PointShape(-4, -4, null), new PointShape(4, 4, null))
-        {
-            Style = new DrawStyle(new DrawColor(255, 255, 255, 0), new DrawColor(255, 255, 255, 0), 2.0, false, true)
-        };
-
-        public static void DrawEllipseAt(object dc, ShapeRenderer r, PointShape s, double radius)
-        {
-            Ellipse.TopLeft.X = s.X - radius;
-            Ellipse.TopLeft.Y = s.Y - radius;
-            Ellipse.BottomRight.X = s.X + radius;
-            Ellipse.BottomRight.Y = s.Y + radius;
-            Ellipse.Draw(dc, r, 0.0, 0.0);
-        }
-    }
-
     public class LineHelper : CommonHelper
     {
         public static void Draw(object dc, ShapeRenderer r, LineShape line)
