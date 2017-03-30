b802c9330d7282462308a513974903aa9d246d79
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2017-03-30 18:34:26 +0200

    Moved code to core project

---
+++ src/Draw2D.Core/ForEachExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Draw2D
+{
+    public static class ForEachExtension
+    {
+        public static void ForEach<T>(this IList<T> list, Action<T> action)
+        {
+            foreach (T t in list)
+            {
+                action(t);
+            }
+        }
+    }
+}
