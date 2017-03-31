883f2b2e8ba91e3583c6c85fe8276bf88fb2ef27
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2017-03-31 18:12:47 +0200

    Added common assembly info

---
+++ src/CommonAssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany("Wiesław Šoltés")]
+[assembly: AssemblyCopyright("Copyright © Wiesław Šoltés 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
