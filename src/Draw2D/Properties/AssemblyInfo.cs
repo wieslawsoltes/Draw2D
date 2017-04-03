e84fcefc563274cae5aa148fd71ae2104552d1a3
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2017-04-03 22:50:45 +0200

    Refactored core project

--- src/Draw2D/Properties/AssemblyInfo.cs
+++ src/Draw2D/Properties/AssemblyInfo.cs
@@ -4,10 +4,19 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Windows;
 
-[assembly: AssemblyTitle("Draw2D.Wpf")]
+[assembly: AssemblyCompany("Wiesław Šoltés")]
+[assembly: AssemblyCopyright("Copyright © Wiesław Šoltés 2017")]
+[assembly: AssemblyTrademark("")]
+
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
+
+[assembly: AssemblyTitle("Draw2D")]
 [assembly: AssemblyDescription("A 2D vector diagram editor.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyProduct("Draw2D.Wpf")]
+[assembly: AssemblyProduct("Draw2D")]
 [assembly: AssemblyCulture("")]
+
 [assembly: ComVisible(false)]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
