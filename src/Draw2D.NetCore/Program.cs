309d57bd4e7b8b1b1b203d1004c91c40554548eb
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2017-04-04 11:42:09 +0200

    Create Program.cs
    [skip ci]

---
+++ src/Draw2D.NetCore/Program.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+using System.Linq;
+using Avalonia;
+using Avalonia.Logging.Serilog;
+using Serilog;
+
+namespace Core2D.Avalonia.NetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InitializeLogging();
+
+            AppBuilder.Configure<App>()
+                .UsePlatformDetect()
+                .Start<MainWindow>();
+        }
+
+        static void InitializeLogging()
+        {
+#if DEBUG
+            SerilogLogger.Initialize(new LoggerConfiguration()
+                .MinimumLevel.Warning()
+                .WriteTo.Trace(outputTemplate: "{Area}: {Message}")
+                .CreateLogger());
+#endif
+        }
+    }
+}
