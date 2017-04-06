4b4d0b589e2117d311c8f26a53e1df16ec5d3470
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2017-04-06 14:25:34 +0200

    Update NetCore-Publish.ps1

--- NetCore-Publish.ps1
+++ NetCore-Publish.ps1
@@ -1,7 +1,8 @@
-cd src/Draw2D.NetCore
+Push-Location -Path "src/Draw2D.NetCore"
 dotnet restore
 dotnet publish -c Release -r win7-x64 -o bin/win7-x64
 dotnet publish -c Release -r win7-x86 -o bin/win7-x86
 dotnet publish -c Release -r ubuntu.14.04-x64 -o bin/ubuntu.14.04-x64
 dotnet publish -c Release -r ubuntu.16.10-x64 -o bin/ubuntu.16.10-x64
 dotnet publish -c Release -r osx.10.12-x64 -o bin/osx.10.12-x64
+Pop-Location
