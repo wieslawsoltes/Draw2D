86ffd2b69198c8bd843947c267316cb90ee85736
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2017-04-08 13:08:04 +0200

    Refactored rectangle tool

--- src/Draw2D.Core/Editor/Tools/RectangleTool.cs
+++ src/Draw2D.Core/Editor/Tools/RectangleTool.cs
@@ -16,42 +16,97 @@ namespace Draw2D.Core.Editor.Tools
 
         public RectangleToolSettings Settings { get; set; }
 
+        private void TopLeftInternal(IToolContext context, double x, double y, Modifier modifier)
+        {
+            Filters?.Any(f => f.Process(context, ref x, ref y));
+
+            _rectangle = new RectangleShape()
+            {
+                TopLeft = context.GetNextPoint(x, y, Settings?.ConnectPoints ?? false, Settings?.HitTestRadius ?? 7.0),
+                BottomRight = context.GetNextPoint(x, y, false, 0.0),
+                Style = context.CurrentStyle
+            };
+            context.WorkingContainer.Shapes.Add(_rectangle);
+            context.Selected.Add(_rectangle.TopLeft);
+            context.Selected.Add(_rectangle.BottomRight);
+
+            context.Capture();
+            context.Invalidate();
+
+            CurrentState = State.BottomRight;
+        }
+
+        private void BottomRightInternal(IToolContext context, double x, double y, Modifier modifier)
+        {
+            Filters?.Any(f => f.Process(context, ref x, ref y));
+
+            CurrentState = State.TopLeft;
+
+            context.Selected.Remove(_rectangle.BottomRight);
+            _rectangle.BottomRight = context.GetNextPoint(x, y, Settings?.ConnectPoints ?? false, Settings?.HitTestRadius ?? 7.0);
+            _rectangle.BottomRight.Y = y;
+            context.WorkingContainer.Shapes.Remove(_rectangle);
+            context.Selected.Remove(_rectangle.TopLeft);
+            context.CurrentContainer.Shapes.Add(_rectangle);
+            _rectangle = null;
+
+            Filters?.ForEach(f => f.Clear(context));
+
+            context.Release();
+            context.Invalidate();
+        }
+
+        private void MoveTopLeftInternal(IToolContext context, double x, double y, Modifier modifier)
+        {
+            Filters?.ForEach(f => f.Clear(context));
+            Filters?.Any(f => f.Process(context, ref x, ref y));
+
+            context.Invalidate();
+        }
+
+        private void MoveBottomRightInternal(IToolContext context, double x, double y, Modifier modifier)
+        {
+            Filters?.ForEach(f => f.Clear(context));
+            Filters?.Any(f => f.Process(context, ref x, ref y));
+
+            _rectangle.BottomRight.X = x;
+            _rectangle.BottomRight.Y = y;
+
+            context.Invalidate();
+        }
+
+        private void CleanInternal(IToolContext context)
+        {
+            CurrentState = State.TopLeft;
+
+            Filters?.ForEach(f => f.Clear(context));
+
+            if (_rectangle != null)
+            {
+                context.WorkingContainer.Shapes.Remove(_rectangle);
+                context.Selected.Remove(_rectangle.TopLeft);
+                context.Selected.Remove(_rectangle.BottomRight);
+                _rectangle = null;
+            }
+
+            context.Release();
+            context.Invalidate();
+        }
+
         public override void LeftDown(IToolContext context, double x, double y, Modifier modifier)
         {
             base.LeftDown(context, x, y, modifier);
-
-            Filters?.Any(f => f.Process(context, ref x, ref y));
 
             switch (CurrentState)
             {
                 case State.TopLeft:
                     {
-                        _rectangle = new RectangleShape()
-                        {
-                            TopLeft = context.GetNextPoint(x, y, Settings?.ConnectPoints ?? false, Settings?.HitTestRadius ?? 7.0),
-                            BottomRight = context.GetNextPoint(x, y, false, 0.0),
-                            Style = context.CurrentStyle
-                        };
-                        context.WorkingContainer.Shapes.Add(_rectangle);
-                        context.Selected.Add(_rectangle.TopLeft);
-                        context.Selected.Add(_rectangle.BottomRight);
-                        context.Capture();
-                        context.Invalidate();
-                        CurrentState = State.BottomRight;
+                        TopLeftInternal(context, x, y, modifier);
                     }
                     break;
                 case State.BottomRight:
                     {
-                        CurrentState = State.TopLeft;
-                        context.Selected.Remove(_rectangle.BottomRight);
-                        _rectangle.BottomRight = context.GetNextPoint(x, y, Settings?.ConnectPoints ?? false, Settings?.HitTestRadius ?? 7.0);
-                        _rectangle.BottomRight.Y = y;
-                        context.WorkingContainer.Shapes.Remove(_rectangle);
-                        context.Selected.Remove(_rectangle.TopLeft);
-                        context.CurrentContainer.Shapes.Add(_rectangle);
-                        _rectangle = null;
-                        context.Release();
-                        context.Invalidate();
+                        BottomRightInternal(context, x, y, modifier);
                     }
                     break;
             }
@@ -75,16 +130,16 @@ namespace Draw2D.Core.Editor.Tools
         {
             base.Move(context, x, y, modifier);
 
-            Filters?.ForEach(f => f.Clear(context));
-            Filters?.Any(f => f.Process(context, ref x, ref y));
-
             switch (CurrentState)
             {
+                case State.TopLeft:
+                    {
+                        MoveTopLeftInternal(context, x, y, modifier);
+                    }
+                    break;
                 case State.BottomRight:
                     {
-                        _rectangle.BottomRight.X = x;
-                        _rectangle.BottomRight.Y = y;
-                        context.Invalidate();
+                        MoveBottomRightInternal(context, x, y, modifier);
                     }
                     break;
             }
@@ -94,20 +149,7 @@ namespace Draw2D.Core.Editor.Tools
         {
             base.Clean(context);
 
-            CurrentState = State.TopLeft;
-
-            Filters?.ForEach(f => f.Clear(context));
-
-            if (_rectangle != null)
-            {
-                context.WorkingContainer.Shapes.Remove(_rectangle);
-                context.Selected.Remove(_rectangle.TopLeft);
-                context.Selected.Remove(_rectangle.BottomRight);
-                _rectangle = null;
-            }
-
-            context.Release();
-            context.Invalidate();
+            CleanInternal(context);
         }
     }
 }
