ed0a0a3c11acbf4042fba65d16784ed5a7071283
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2017-04-08 13:38:27 +0200

    Refactored scribble tool

--- src/Draw2D.Core/Editor/Tools/ScribbleTool.cs
+++ src/Draw2D.Core/Editor/Tools/ScribbleTool.cs
@@ -21,25 +21,97 @@ namespace Draw2D.Core.Editor.Tools
 
         public ScribbleToolSettings Settings { get; set; }
 
+        private void StartInternal(IToolContext context, double x, double y, Modifier modifier)
+        {
+            Filters?.Any(f => f.Process(context, ref x, ref y));
+
+            _scribble = new ScribbleShape()
+            {
+                Style = context.CurrentStyle
+            };
+            _scribble.Points.Add(new PointShape(x, y, context.PointShape));
+            context.WorkingContainer.Shapes.Add(_scribble);
+
+            context.Capture();
+            context.Invalidate();
+
+            CurrentState = State.Points;
+        }
+
+        private void PointsInternal(IToolContext context, double x, double y, Modifier modifier)
+        {
+            CurrentState = State.Start;
+
+            if (Settings?.Simplify ?? true)
+            {
+                List<Vector2> points = _scribble.Points.Select(p => new Vector2((float)p.X, (float)p.Y)).ToList();
+                int count = _scribble.Points.Count;
+                RDP rdp = new RDP();
+                BitArray accepted = rdp.DouglasPeucker(points, 0, count - 1, Settings?.Epsilon ?? 1.0);
+                int removed = 0;
+                for (int i = 0; i <= count - 1; ++i)
+                {
+                    if (!accepted[i])
+                    {
+                        _scribble.Points.RemoveAt(i - removed);
+                        ++removed;
+                    }
+                }
+            }
+            context.WorkingContainer.Shapes.Remove(_scribble);
+            context.CurrentContainer.Shapes.Add(_scribble);
+
+            _scribble = null;
+
+            Filters?.ForEach(f => f.Clear(context));
+
+            context.Release();
+            context.Invalidate();
+        }
+
+        private void MoveStartInternal(IToolContext context, double x, double y, Modifier modifier)
+        {
+            Filters?.ForEach(f => f.Clear(context));
+            Filters?.Any(f => f.Process(context, ref x, ref y));
+
+            context.Invalidate();
+        }
+
+        private void MovePointsInternal(IToolContext context, double x, double y, Modifier modifier)
+        {
+            Filters?.ForEach(f => f.Clear(context));
+            Filters?.Any(f => f.Process(context, ref x, ref y));
+
+            _scribble.Points.Add(new PointShape(x, y, context.PointShape));
+
+            context.Invalidate();
+        }
+
+        private void CleanInternal(IToolContext context)
+        {
+            CurrentState = State.Start;
+
+            Filters?.ForEach(f => f.Clear(context));
+
+            if (_scribble != null)
+            {
+                context.WorkingContainer.Shapes.Remove(_scribble);
+                _scribble = null;
+            }
+
+            context.Release();
+            context.Invalidate();
+        }
+
         public override void LeftDown(IToolContext context, double x, double y, Modifier modifier)
         {
             base.LeftDown(context, x, y, modifier);
-
-            Filters?.Any(f => f.Process(context, ref x, ref y));
 
             switch (CurrentState)
             {
                 case State.Start:
                     {
-                        _scribble = new ScribbleShape()
-                        {
-                            Style = context.CurrentStyle
-                        };
-                        _scribble.Points.Add(new PointShape(x, y, context.PointShape));
-                        context.WorkingContainer.Shapes.Add(_scribble);
-                        context.Capture();
-                        context.Invalidate();
-                        CurrentState = State.Points;
+                        StartInternal(context, x, y, modifier);
                     }
                     break;
             }
@@ -53,28 +125,7 @@ namespace Draw2D.Core.Editor.Tools
             {
                 case State.Points:
                     {
-                        CurrentState = State.Start;
-                        if (Settings?.Simplify ?? true)
-                        {
-                            List<Vector2> points = _scribble.Points.Select(p => new Vector2((float)p.X, (float)p.Y)).ToList();
-                            int count = _scribble.Points.Count;
-                            RDP rdp = new RDP();
-                            BitArray accepted = rdp.DouglasPeucker(points, 0, count - 1, Settings?.Epsilon ?? 1.0);
-                            int removed = 0;
-                            for (int i = 0; i <= count - 1; ++i)
-                            {
-                                if (!accepted[i])
-                                {
-                                    _scribble.Points.RemoveAt(i - removed);
-                                    ++removed;
-                                }
-                            }
-                        }
-                        context.WorkingContainer.Shapes.Remove(_scribble);
-                        context.CurrentContainer.Shapes.Add(_scribble);
-                        _scribble = null;
-                        context.Release();
-                        context.Invalidate();
+                        PointsInternal(context, x, y, modifier);
                     }
                     break;
             }
@@ -98,15 +149,16 @@ namespace Draw2D.Core.Editor.Tools
         {
             base.Move(context, x, y, modifier);
 
-            Filters?.ForEach(f => f.Clear(context));
-            Filters?.Any(f => f.Process(context, ref x, ref y));
-
             switch (CurrentState)
             {
+                case State.Start:
+                    {
+                        MoveStartInternal(context, x, y, modifier);
+                    }
+                    break;
                 case State.Points:
                     {
-                        _scribble.Points.Add(new PointShape(x, y, context.PointShape));
-                        context.Invalidate();
+                        MovePointsInternal(context, x, y, modifier);
                     }
                     break;
             }
@@ -116,18 +168,7 @@ namespace Draw2D.Core.Editor.Tools
         {
             base.Clean(context);
 
-            CurrentState = State.Start;
-
-            Filters?.ForEach(f => f.Clear(context));
-
-            if (_scribble != null)
-            {
-                context.WorkingContainer.Shapes.Remove(_scribble);
-                _scribble = null;
-            }
-
-            context.Release();
-            context.Invalidate();
+            CleanInternal(context);
         }
     }
 }
