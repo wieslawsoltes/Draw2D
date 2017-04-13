3401db182a6fffe9346c7732b5462fad74938639
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2017-04-13 10:19:06 +0200

    Update ShapesContainerRenderView.cs

--- src/Draw2D.Wpf/Controls/ShapesContainerRenderView.cs
+++ src/Draw2D.Wpf/Controls/ShapesContainerRenderView.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Media;
 using Draw2D.Core.ViewModels.Containers;
 
