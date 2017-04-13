b98d7460df93ccce345965753df882b74d4468a0
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2017-04-13 10:18:21 +0200

    Update ShapesContainerRenderView.cs

--- src/Draw2D.Wpf/Controls/ShapesContainerRenderView.cs
+++ src/Draw2D.Wpf/Controls/ShapesContainerRenderView.cs
@@ -8,13 +8,35 @@ namespace Draw2D.Wpf.Controls
 {
     public class ShapesContainerRenderView : Canvas
     {
+        private bool _drawWorking = false;
+
+        protected override void OnMouseEnter(MouseEventArgs e)
+        {
+            base.OnMouseEnter(e);
+            _drawWorking = true;
+            this.InvalidateVisual();
+        }
+
+        protected override void OnMouseLeave(MouseEventArgs e)
+        {
+            base.OnMouseLeave(e);
+            _drawWorking = false;
+            this.InvalidateVisual();
+        }
+
         protected override void OnRender(DrawingContext dc)
         {
             base.OnRender(dc);
 
             if (this.DataContext is ShapesContainerViewModel vm)
             {
-                vm.Presenter.Draw(dc, vm);
+                vm.Presenter.DrawContent(dc, vm);
+
+                if (_drawWorking)
+                {
+                    vm.Presenter.DrawWorking(dc, vm);
+                }
+
                 vm.Presenter.DrawHelpers(dc, vm);
             }
         }
