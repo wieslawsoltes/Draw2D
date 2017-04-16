e91660a22ef0b5594250a650ab11cc16e8e04b70
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2017-04-16 17:38:36 +0200

    Moved Main to App class

--- src/Draw2D.Avalonia/App.xaml.cs
+++ src/Draw2D.Avalonia/App.xaml.cs
@@ -3,16 +3,39 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Input;
+using Avalonia.Logging.Serilog;
 using Avalonia.Markup.Xaml;
 using Draw2D.Avalonia.Controls;
 using Draw2D.Avalonia.Renderers;
 using Draw2D.Avalonia.Views;
 using Draw2D.ViewModels;
+using Serilog;
 
 namespace Draw2D.Avalonia
 {
     public class App : Application
     {
+        static void Main(string[] args)
+        {
+            InitializeLogging();
+
+            var app = new App();
+            AppBuilder.Configure(app)
+                .UsePlatformDetect()
+                .SetupWithoutStarting();
+            app.Start();
+        }
+
+        static void InitializeLogging()
+        {
+#if DEBUG
+            SerilogLogger.Initialize(new LoggerConfiguration()
+                .MinimumLevel.Warning()
+                .WriteTo.Trace(outputTemplate: "{Area}: {Message}")
+                .CreateLogger());
+#endif
+        }
+
         public override void Initialize()
         {
             AvaloniaXamlLoader.Load(this);
