ff7547d17f70d4aba3dab1fdf5975a8beffa4db8
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2017-04-16 17:40:04 +0200

    Removed empty lines

--- src/Draw2D.Wpf/Properties/AssemblyInfo.cs
+++ src/Draw2D.Wpf/Properties/AssemblyInfo.cs
@@ -7,16 +7,13 @@ using System.Windows;
 [assembly: AssemblyCompany("Wiesław Šoltés")]
 [assembly: AssemblyCopyright("Copyright © Wiesław Šoltés 2017")]
 [assembly: AssemblyTrademark("")]
-
 [assembly: AssemblyVersion("1.0.0")]
 [assembly: AssemblyFileVersion("1.0.0")]
 [assembly: AssemblyInformationalVersion("1.0.0")]
-
 [assembly: AssemblyTitle("Draw2D")]
 [assembly: AssemblyDescription("A 2D vector diagram editor.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyProduct("Draw2D")]
 [assembly: AssemblyCulture("")]
-
 [assembly: ComVisible(false)]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
