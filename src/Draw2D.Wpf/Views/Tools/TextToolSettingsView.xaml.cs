5f260c644a6de2c43f1cf8d049b7a69df0afb5cd
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2017-04-18 23:18:43 +0200

    Added TextToolSettings view

---
+++ src/Draw2D.Wpf/Views/Tools/TextToolSettingsView.xaml.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System.Windows.Controls;
+
+namespace Draw2D.Wpf.Views.Tools.Settings
+{
+    public partial class TextToolSettingsView : UserControl
+    {
+        public TextToolSettingsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
