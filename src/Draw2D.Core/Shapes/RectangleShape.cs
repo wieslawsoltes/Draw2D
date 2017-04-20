a6f086eb32e9a059ff799dc76085ef470019235a
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2017-04-20 10:30:33 +0200

    Update RectangleShape.cs
    [skip ci]

--- src/Draw2D.Core/Shapes/RectangleShape.cs
+++ src/Draw2D.Core/Shapes/RectangleShape.cs
@@ -6,51 +6,21 @@ using Draw2D.Core.Renderers;
 
 namespace Draw2D.Core.Shapes
 {
-    public class RectangleShape : ConnectableShape, ICopyable
+    public class RectangleShape : BoxShape, ICopyable
     {
-        private PointShape _topLeft;
-        private PointShape _bottomRight;
-
-        public PointShape TopLeft
-        {
-            get => _topLeft;
-            set => Update(ref _topLeft, value);
-        }
-
-        public PointShape BottomRight
-        {
-            get => _bottomRight;
-            set => Update(ref _bottomRight, value);
-        }
-
         public RectangleShape()
             : base()
         {
         }
 
         public RectangleShape(PointShape topLeft, PointShape bottomRight)
-            : base()
+            : base(topLeft, bottomRight)
         {
-            this.TopLeft = topLeft;
-            this.BottomRight = bottomRight;
-        }
-
-        public override IEnumerable<PointShape> GetPoints()
-        {
-            yield return TopLeft;
-            yield return BottomRight;
-            foreach (var point in Points)
-            {
-                yield return point;
-            }
         }
 
         public override bool Invalidate(ShapeRenderer r, double dx, double dy)
         {
             bool result = base.Invalidate(r, dx, dy);
-
-            result |= _topLeft?.Invalidate(r, dx, dy) ?? false;
-            result |= _bottomRight?.Invalidate(r, dx, dy) ?? false;
 
             if (this.IsDirty || result == true)
             {
@@ -83,110 +53,6 @@ namespace Draw2D.Core.Shapes
 
             base.Draw(dc, r, dx, dy);
             base.EndTransform(dc, r);
-        }
-
-        public override void Move(ISet<ShapeObject> selected, double dx, double dy)
-        {
-            if (!selected.Contains(_topLeft))
-            {
-                _topLeft.Move(selected, dx, dy);
-            }
-
-            if (!selected.Contains(_bottomRight))
-            {
-                _bottomRight.Move(selected, dx, dy);
-            }
-
-            base.Move(selected, dx, dy);
-        }
-
-        public override void Select(ISet<ShapeObject> selected)
-        {
-            base.Select(selected);
-            TopLeft.Select(selected);
-            BottomRight.Select(selected);
-        }
-
-        public override void Deselect(ISet<ShapeObject> selected)
-        {
-            base.Deselect(selected);
-            TopLeft.Deselect(selected);
-            BottomRight.Deselect(selected);
-        }
-
-        private bool CanConnect(PointShape point)
-        {
-            return TopLeft != point
-                && BottomRight != point;
-        }
-
-        public override bool Connect(PointShape point, PointShape target)
-        {
-            if (base.Connect(point, target))
-            {
-                return true;
-            }
-            else if (CanConnect(point))
-            {
-                if (TopLeft == target)
-                {
-                    Debug.WriteLine($"{nameof(RectangleShape)}: Connected to {nameof(TopLeft)}");
-                    this.TopLeft = point;
-                    return true;
-                }
-                else if (BottomRight == target)
-                {
-                    Debug.WriteLine($"{nameof(RectangleShape)}: Connected to {nameof(BottomRight)}");
-                    this.BottomRight = point;
-                    return true;
-                }
-            }
-            return false;
-        }
-
-        public override bool Disconnect(PointShape point, out PointShape result)
-        {
-            if (base.Disconnect(point, out result))
-            {
-                return true;
-            }
-            else if (TopLeft == point)
-            {
-                Debug.WriteLine($"{nameof(RectangleShape)}: Disconnected from {nameof(TopLeft)}");
-                result = (PointShape)point.Copy(null);
-                this.TopLeft = result;
-                return true;
-            }
-            else if (BottomRight == point)
-            {
-                Debug.WriteLine($"{nameof(RectangleShape)}: Disconnected from {nameof(BottomRight)}");
-                result = (PointShape)point.Copy(null);
-                this.BottomRight = result;
-                return true;
-            }
-            result = null;
-            return false;
-        }
-
-        public override bool Disconnect()
-        {
-            bool result = base.Disconnect();
-
-            if (this.TopLeft != null)
-            {
-                Debug.WriteLine($"{nameof(RectangleShape)}: Disconnected from {nameof(TopLeft)}");
-                this.TopLeft = (PointShape)this.TopLeft.Copy(null);
-                result = true;
-            }
-
-            if (this.BottomRight != null)
-            {
-                Debug.WriteLine($"{nameof(RectangleShape)}: Disconnected from {nameof(BottomRight)}");
-                this.BottomRight = (PointShape)this.BottomRight.Copy(null);
-                result = true;
-            }
-
-            return result;
         }
 
         public object Copy(IDictionary<object, object> shared)
