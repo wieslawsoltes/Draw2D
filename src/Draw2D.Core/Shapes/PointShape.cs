c146f0f535b86f35c732fc3132989512960e73d1
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2017-04-20 13:44:09 +0200

    Update PointShape.cs
    [skip ci]

--- src/Draw2D.Core/Shapes/PointShape.cs
+++ src/Draw2D.Core/Shapes/PointShape.cs
@@ -5,7 +5,7 @@ using Draw2D.Core.Renderers;
 
 namespace Draw2D.Core.Shapes
 {
-    public class PointShape : ShapeObject, IPoint, ICopyable
+    public class PointShape : ShapeObject, ICopyable
     {
         private double _x;
         private double _y;
