3b1d76b9f8fb3cceca1204ec56d39a951dcc114c
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2017-04-20 14:33:06 +0200

    Update EllipseHitTest.cs
    [skip ci]

--- src/Draw2D.Editor/Bounds/Shapes/EllipseHitTest.cs
+++ src/Draw2D.Editor/Bounds/Shapes/EllipseHitTest.cs
@@ -7,63 +7,8 @@ using Draw2D.Spatial;
 
 namespace Draw2D.Editor.Bounds.Shapes
 {
-    public class EllipseHitTest : HitTestBase
+    public class EllipseHitTest : BoxHitTest
     {
         public override Type TargetType => typeof(EllipseShape);
-
-        public override PointShape TryToGetPoint(ShapeObject shape, Point2 target, double radius, IHitTest hitTest)
-        {
-            var ellipse = shape as EllipseShape;
-            if (ellipse == null)
-                throw new ArgumentNullException("shape");
-
-            var pointHitTest = hitTest.Registered[typeof(PointShape)];
-
-            if (pointHitTest.TryToGetPoint(ellipse.TopLeft, target, radius, hitTest) != null)
-            {
-                return ellipse.TopLeft;
-            }
-
-            if (pointHitTest.TryToGetPoint(ellipse.BottomRight, target, radius, hitTest) != null)
-            {
-                return ellipse.BottomRight;
-            }
-
-            foreach (var point in ellipse.Points)
-            {
-                if (pointHitTest.TryToGetPoint(point, target, radius, hitTest) != null)
-                {
-                    return point;
-                }
-            }
-
-            return null;
-        }
-
-        public override ShapeObject Contains(ShapeObject shape, Point2 target, double radius, IHitTest hitTest)
-        {
-            var ellipse = shape as EllipseShape;
-            if (ellipse == null)
-                throw new ArgumentNullException("shape");
-
-            return Rect2.FromPoints(
-                ellipse.TopLeft.X,
-                ellipse.TopLeft.Y,
-                ellipse.BottomRight.X,
-                ellipse.BottomRight.Y).Contains(target) ? shape : null;
-        }
-
-        public override ShapeObject Overlaps(ShapeObject shape, Rect2 target, double radius, IHitTest hitTest)
-        {
-            var ellipse = shape as EllipseShape;
-            if (ellipse == null)
-                throw new ArgumentNullException("shape");
-
-            return Rect2.FromPoints(
-                ellipse.TopLeft.X,
-                ellipse.TopLeft.Y,
-                ellipse.BottomRight.X,
-                ellipse.BottomRight.Y).IntersectsWith(target) ? shape : null;
-        }
     }
 }
