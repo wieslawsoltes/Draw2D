01829f3a614964bef76476aff4355f3b3e595378
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2017-04-20 11:10:13 +0200

    Update ShapeRenderer.cs
    [skip ci]

--- src/Draw2D.Core/Renderers/ShapeRenderer.cs
+++ src/Draw2D.Core/Renderers/ShapeRenderer.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 using System.Collections.Generic;
-using Draw2D.Core.Shapes;
 using Draw2D.Core.Style;
 
 namespace Draw2D.Core.Renderers
