7803c591269aae365d37d0599050ef59db2f0ea4
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2017-08-09 21:27:56 +0200

    Removed wpf version

--- NetCore-Publish.ps1
+++ NetCore-Publish.ps1
@@ -1,8 +1,6 @@
 Push-Location -Path "src/Core2D.Avalonia"
 dotnet restore
 dotnet publish -c Release -r win7-x64 -o bin/win7-x64
-dotnet publish -c Release -r win7-x86 -o bin/win7-x86
 dotnet publish -c Release -r ubuntu.14.04-x64 -o bin/ubuntu.14.04-x64
-dotnet publish -c Release -r ubuntu.16.10-x64 -o bin/ubuntu.16.10-x64
 dotnet publish -c Release -r osx.10.12-x64 -o bin/osx.10.12-x64
 Pop-Location
