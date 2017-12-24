2275d19a53495ae79a9480e2ef73332daa80a98d
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2017-12-24 13:41:39 +0100

    Fixed paths

--- NetCore-Patch.cmd
+++ NetCore-Patch.cmd
@@ -1,8 +1,8 @@
 @echo off
 setlocal
-set msvcp140_x64=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Redist\MSVC\14.11.25325\x64\Microsoft.VC141.CRT\msvcp140.dll
-set vcruntime140_x64=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Redist\MSVC\14.11.25325\x64\Microsoft.VC141.CRT\vcruntime140.dll
-set editbin=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.11.25503\bin\HostX86\x86\editbin.exe
+set msvcp140_x64=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Redist\MSVC\14.12.25810\x64\Microsoft.VC141.CRT\msvcp140.dll
+set vcruntime140_x64=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Redist\MSVC\14.12.25810\x64\Microsoft.VC141.CRT\vcruntime140.dll
+set editbin=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.12.25827\bin\HostX86\x86\editbin.exe
 set win7_x64=src\Core2D.Avalonia\bin\win7-x64
 set executable=Core2D.Avalonia.exe
 copy /Y "%msvcp140_x64%" "%win7_x64%"
