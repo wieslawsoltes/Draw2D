2bc1aa079be1ba6bffe0d8fe2afd37ae21b8bc36
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2018-04-10 20:32:36 +0200

    Added program class

--- src/Core2D.Avalonia/App.xaml.cs
+++ src/Core2D.Avalonia/App.xaml.cs
@@ -1,11 +1,7 @@
 ﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
-using System;
 using Avalonia;
-using Avalonia.Logging.Serilog;
 using Avalonia.Markup.Xaml;
-using Core2D.Avalonia.Renderers;
-using Core2D.ViewModels;
 
 namespace Core2D.Avalonia
 {
@@ -14,48 +10,6 @@ namespace Core2D.Avalonia
         public override void Initialize()
         {
             AvaloniaXamlLoader.Load(this);
-        }
-
-        static object BuildDataContext()
-        {
-            var bootstrapper = new Bootstrapper();
-            var vm = bootstrapper.CreateDemoViewModel();
-
-            bootstrapper.CreateDemoContainer(vm);
-
-            vm.Renderer = new AvaloniaShapeRenderer();
-            vm.Selected = vm.Renderer.Selected;
-
-            return vm;
-        }
-
-        public static AppBuilder BuildAvaloniaApp()
-            => AppBuilder.Configure<App>()
-                         .UsePlatformDetect()
-                         //.UseDirect2D1()
-                         //.UseSkia()
-                         .LogToDebug();
-
-        static void Print(Exception ex)
-        {
-            Console.WriteLine(ex.Message);
-            Console.WriteLine(ex.StackTrace);
-            if (ex.InnerException != null)
-            {
-                Print(ex.InnerException);
-            }
-        }
-
-        static void Main(string[] args)
-        {
-            try
-            {
-                BuildAvaloniaApp().Start<MainWindow>(() => BuildDataContext());
-            }
-            catch (Exception ex)
-            {
-                Print(ex);
-            }
         }
     }
 }
---
+++ src/Core2D.Avalonia/Program.cs
@@ -0,0 +1,53 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+using Avalonia;
+using Avalonia.Logging.Serilog;
+using Core2D.Avalonia.Renderers;
+using Core2D.ViewModels;
+
+namespace Core2D.Avalonia
+{
+    class Program
+    {
+        static void Print(Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            Console.WriteLine(ex.StackTrace);
+            if (ex.InnerException != null)
+            {
+                Print(ex.InnerException);
+            }
+        }
+
+        static object DataContextProvider()
+        {
+            var bootstrapper = new Bootstrapper();
+            var vm = bootstrapper.CreateDemoViewModel();
+
+            bootstrapper.CreateDemoContainer(vm);
+
+            vm.Renderer = new AvaloniaShapeRenderer();
+            vm.Selected = vm.Renderer.Selected;
+
+            return vm;
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                BuildAvaloniaApp().Start<MainWindow>(() => DataContextProvider());
+            }
+            catch (Exception ex)
+            {
+                Print(ex);
+            }
+        }
+
+        public static AppBuilder BuildAvaloniaApp()
+            => AppBuilder.Configure<App>()
+                .UsePlatformDetect()
+                .LogToDebug();
+    }
+}
