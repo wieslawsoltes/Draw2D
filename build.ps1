cad9c579f6e95a9325a3b8394d6e3133f4c9cbe5
wieslawsoltes <wieslaw.soltes@gmail.com>
2018-04-13 06:56:01 +0000

    Fix build script

--- build.ps1
+++ build.ps1
@@ -1,7 +1,7 @@
 dotnet restore src/Core2D.Avalonia -v m
 dotnet build src/Core2D.Avalonia/Core2D.Avalonia.csproj -v m
 #dotnet publish src/Core2D.Avalonia/Core2D.Avalonia.csproj -c Release -r win7-x64 -o bin/win7-x64s /p:CoreRT=False
-#dotnet publish src/Core2D.Avalonia/Core2D.Avalonia.csproj-c Release -r ubuntu.14.04-x64 -o bin/ubuntu.14.04-x64 -v m /p:CoreRT=False
-#dotnet publish src/Core2D.Avalonia/Core2D.Avalonia.csproj-c Release -r osx.10.12-x64 -o bin/osx.10.12-x64 -v m /p:CoreRT=False
-dotnet publish src/Core2D.Avalonia/Core2D.Avalonia.csproj-c Debug -r win-x64 -o bin/win-x64-debug -v m /p:CoreRT=True
-dotnet publish src/Core2D.Avalonia/Core2D.Avalonia.csproj-c Release -r win-x64 -o bin/win-x64-release -v m /p:CoreRT=True
+#dotnet publish src/Core2D.Avalonia/Core2D.Avalonia.csproj -c Release -r ubuntu.14.04-x64 -o bin/ubuntu.14.04-x64 -v m /p:CoreRT=False
+#dotnet publish src/Core2D.Avalonia/Core2D.Avalonia.csproj -c Release -r osx.10.12-x64 -o bin/osx.10.12-x64 -v m /p:CoreRT=False
+dotnet publish src/Core2D.Avalonia/Core2D.Avalonia.csproj -c Debug -r win-x64 -o bin/win-x64-debug -v m /p:CoreRT=True
+dotnet publish src/Core2D.Avalonia/Core2D.Avalonia.csproj -c Release -r win-x64 -o bin/win-x64-release -v m /p:CoreRT=True
