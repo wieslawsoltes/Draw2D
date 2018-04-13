c645065c610e9f4ce72963ee3c728c59f9cde746
wieslawsoltes <wieslaw.soltes@gmail.com>
2018-04-13 07:16:38 +0000

    Added error checking to build script

--- build.sh
+++ build.sh
@@ -1,6 +1,26 @@
 #!/usr/bin/env bash
+
 dotnet restore src/Core2D.Avalonia -v m
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
 dotnet build src/Core2D.Avalonia/Core2D.Avalonia.csproj -v m
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
 dotnet publish src/Core2D.Avalonia/Core2D.Avalonia.csproj -c Release -r win7-x64 -o bin/win7-x64 -v m /p:CoreRT=False
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
 dotnet publish src/Core2D.Avalonia/Core2D.Avalonia.csproj -c Release -r ubuntu.14.04-x64 -o bin/ubuntu.14.04-x64 -v m /p:CoreRT=False
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
 dotnet publish src/Core2D.Avalonia/Core2D.Avalonia.csproj -c Release -r osx.10.12-x64 -o bin/osx.10.12-x64 -v m /p:CoreRT=False
+if [ $? -ne 0 ]; then
+    exit 1
+fi
