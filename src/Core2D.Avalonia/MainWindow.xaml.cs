b1a1a441ec7200583d09daedcb78bd92c7ccab2d
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2018-04-13 23:44:06 +0200

    Added AttachDevTools

--- src/Core2D.Avalonia/MainWindow.xaml.cs
+++ src/Core2D.Avalonia/MainWindow.xaml.cs
@@ -11,6 +11,7 @@ namespace Core2D.Avalonia
         public MainWindow()
         {
             this.InitializeComponent();
+            this.AttachDevTools();
         }
 
         private void InitializeComponent()
