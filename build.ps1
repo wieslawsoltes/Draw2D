4d3ceb755b284a20a8feda20ca5d7eb12fcad82a
wieslawsoltes <wieslaw.soltes@gmail.com>
2018-04-13 07:14:43 +0000

    Added error checking to build script

--- build.ps1
+++ build.ps1
@@ -1,7 +1,20 @@
 dotnet restore src/Core2D.Avalonia -v m
+if ($LastExitCode -ne 0) { Exit -1 }
+
 dotnet build src/Core2D.Avalonia/Core2D.Avalonia.csproj -v m
+if ($LastExitCode -ne 0) { Exit -1 }
+
 #dotnet publish src/Core2D.Avalonia/Core2D.Avalonia.csproj -c Release -r win7-x64 -o bin/win7-x64s /p:CoreRT=False
+#if ($LastExitCode -ne 0) { Exit -1 }
+
 #dotnet publish src/Core2D.Avalonia/Core2D.Avalonia.csproj -c Release -r ubuntu.14.04-x64 -o bin/ubuntu.14.04-x64 -v m /p:CoreRT=False
+#if ($LastExitCode -ne 0) { Exit -1 }
+
 #dotnet publish src/Core2D.Avalonia/Core2D.Avalonia.csproj -c Release -r osx.10.12-x64 -o bin/osx.10.12-x64 -v m /p:CoreRT=False
+#if ($LastExitCode -ne 0) { Exit -1 }
+
 dotnet publish src/Core2D.Avalonia/Core2D.Avalonia.csproj -c Debug -r win-x64 -o bin/win-x64-debug -v m /p:CoreRT=True
+if ($LastExitCode -ne 0) { Exit -1 }
+
 dotnet publish src/Core2D.Avalonia/Core2D.Avalonia.csproj -c Release -r win-x64 -o bin/win-x64-release -v m /p:CoreRT=True
+if ($LastExitCode -ne 0) { Exit -1 }
