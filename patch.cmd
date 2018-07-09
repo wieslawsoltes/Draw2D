93dae31c6118934617597fc0a458fa226a46b3e7
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2018-07-09 20:04:36 +0200

    Updated build script

--- patch.cmd
+++ patch.cmd
@@ -1,9 +1,9 @@
 @echo off
 setlocal
 
-set msvcp140_x64=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Redist\MSVC\14.13.26020\x64\Microsoft.VC141.CRT\msvcp140.dll
-set vcruntime140_x64=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Redist\MSVC\14.13.26020\x64\Microsoft.VC141.CRT\vcruntime140.dll
-set editbin=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.13.26128\bin\HostX86\x86\editbin.exe
+set msvcp140_x64=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Redist\MSVC\14.14.26405\x64\Microsoft.VC141.CRT\msvcp140.dll
+set vcruntime140_x64=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Redist\MSVC\14.14.26405\x64\Microsoft.VC141.CRT\vcruntime140.dll
+set editbin=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.14.26428\bin\HostX86\x86\editbin.exe
 set win7_x64=src\Core2D.Avalonia\bin\win7-x64
 set win_x64_debug=src\Core2D.Avalonia\bin\win-x64-debug
 set win_x64_release=src\Core2D.Avalonia\bin\win-x64-release
