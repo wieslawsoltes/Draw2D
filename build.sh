c6f8265d3a625ec60cebc92a26d8c2cd6a3a8359
wieslawsoltes <wieslaw.soltes@gmail.com>
2018-07-12 09:04:16 +0000

    Refactored build

--- build.sh
+++ build.sh
@@ -1,26 +1,16 @@
 #!/usr/bin/env bash
 
-dotnet restore src/Core2D.Avalonia -v m
+dotnet restore src/Core2D.Avalonia
 if [ $? -ne 0 ]; then
     exit 1
 fi
 
-dotnet build src/Core2D.Avalonia/Core2D.Avalonia.csproj -v m
+dotnet build src/Core2D.Avalonia/Core2D.Avalonia.csproj
 if [ $? -ne 0 ]; then
     exit 1
 fi
 
-dotnet publish src/Core2D.Avalonia/Core2D.Avalonia.csproj -c Release -r win7-x64 -o bin/win7-x64 -v m /p:CoreRT=False
-if [ $? -ne 0 ]; then
-    exit 1
-fi
-
-dotnet publish src/Core2D.Avalonia/Core2D.Avalonia.csproj -c Release -r ubuntu.14.04-x64 -o bin/ubuntu.14.04-x64 -v m /p:CoreRT=False
-if [ $? -ne 0 ]; then
-    exit 1
-fi
-
-dotnet publish src/Core2D.Avalonia/Core2D.Avalonia.csproj -c Release -r osx.10.12-x64 -o bin/osx.10.12-x64 -v m /p:CoreRT=False
+dotnet publish src/Core2D.Avalonia/Core2D.Avalonia.csproj -c Release -r ubuntu.14.04-x64 -o bin/ubuntu.14.04-x64
 if [ $? -ne 0 ]; then
     exit 1
 fi
--- patch.cmd
+++ patch.cmd
@@ -5,23 +5,10 @@ set msvcp140_x64=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\V
 set vcruntime140_x64=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Redist\MSVC\14.14.26405\x64\Microsoft.VC141.CRT\vcruntime140.dll
 set editbin=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.14.26428\bin\HostX86\x86\editbin.exe
 set win7_x64=src\Core2D.Avalonia\bin\win7-x64
-rem set win_x64_debug=src\Core2D.Avalonia\bin\win-x64-debug
-rem set win_x64_release=src\Core2D.Avalonia\bin\win-x64-release
-rem set libSkiaSharp=%UserProfile%\.nuget\packages\skiasharp\1.57.1\runtimes\win7-x64\native\libSkiaSharp.dll
-rem set executable=Core2D.Avalonia.exe
+set executable=Core2D.Avalonia.exe
 
 copy /Y "%msvcp140_x64%" "%win7_x64%"
 copy /Y "%vcruntime140_x64%" "%win7_x64%"
 rem "%editbin%" /subsystem:windows "%win7_x64%\%executable%"
-
-rem copy /Y "%msvcp140_x64%" "%win_x64_debug%"
-rem copy /Y "%vcruntime140_x64%" "%win_x64_debug%"
-rem copy /Y "%libSkiaSharp%" "%win_x64_debug%"
-rem "%editbin%" /subsystem:windows "%win_x64_debug%\%executable%"
-
-rem copy /Y "%msvcp140_x64%" "%win_x64_release%"
-rem copy /Y "%vcruntime140_x64%" "%win_x64_release%"
-rem copy /Y "%libSkiaSharp%" "%win_x64_release%"
-rem "%editbin%" /subsystem:windows "%win_x64_release%\%executable%"
 
 endlocal
