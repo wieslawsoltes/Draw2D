30a7759aeecfa637d146134e9fe7eb0d5e1fbf42
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2019-04-08 08:09:50 +0200

    Delete build.ps1

--- build.ps1
+++
@@ -1,8 +0,0 @@
-dotnet restore src/Core2D.Avalonia
-if ($LastExitCode -ne 0) { Exit 1 }
-
-dotnet build src/Core2D.Avalonia/Core2D.Avalonia.csproj
-if ($LastExitCode -ne 0) { Exit 1 }
-
-dotnet publish src/Core2D.Avalonia/Core2D.Avalonia.csproj -c Release -r win7-x64 -o bin/win7-x64
-if ($LastExitCode -ne 0) { Exit 1 }
