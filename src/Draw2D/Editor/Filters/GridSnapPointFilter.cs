7c53d3555c1c3fa8f22d411097c2f9ac983aa833
wieslawsoltes <wieslaw.soltes@gmail.com>
2019-04-09 08:37:20 +0000

    Refactoring

--- src/Draw2D/Editor/Filters/GridSnapPointFilter.cs
+++ src/Draw2D/Editor/Filters/GridSnapPointFilter.cs
@@ -2,9 +2,56 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 using System.Diagnostics;
 using Draw2D.Shapes;
+using Draw2D.Style;
 
 namespace Draw2D.Editor.Filters
 {
+    public class GridSnapSettings : SettingsBase
+    {
+        private bool _isEnabled;
+        private bool _enableGuides;
+        private GridSnapMode _mode;
+        private double _gridSizeX;
+        private double _gridSizeY;
+        private ShapeStyle _guideStyle;
+
+        public bool IsEnabled
+        {
+            get => _isEnabled;
+            set => Update(ref _isEnabled, value);
+        }
+
+        public bool EnableGuides
+        {
+            get => _enableGuides;
+            set => Update(ref _enableGuides, value);
+        }
+
+        public GridSnapMode Mode
+        {
+            get => _mode;
+            set => Update(ref _mode, value);
+        }
+
+        public double GridSizeX
+        {
+            get => _gridSizeX;
+            set => Update(ref _gridSizeX, value);
+        }
+
+        public double GridSizeY
+        {
+            get => _gridSizeY;
+            set => Update(ref _gridSizeY, value);
+        }
+
+        public ShapeStyle GuideStyle
+        {
+            get => _guideStyle;
+            set => Update(ref _guideStyle, value);
+        }
+    }
+
     public class GridSnapPointFilter : PointFilter
     {
         public override string Title => "Grid-Snap";
--- src/Draw2D/Editor/Filters/LineSnapPointFilter.cs
+++ src/Draw2D/Editor/Filters/LineSnapPointFilter.cs
@@ -4,10 +4,57 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using Draw2D.Shapes;
+using Draw2D.Style;
 using Spatial;
 
 namespace Draw2D.Editor.Filters
 {
+    public class LineSnapSettings : SettingsBase
+    {
+        private bool _isEnabled;
+        private bool _enableGuides;
+        private LineSnapMode _mode;
+        private LineSnapTarget _target;
+        private double _threshold;
+        private ShapeStyle _guideStyle;
+
+        public bool IsEnabled
+        {
+            get => _isEnabled;
+            set => Update(ref _isEnabled, value);
+        }
+
+        public bool EnableGuides
+        {
+            get => _enableGuides;
+            set => Update(ref _enableGuides, value);
+        }
+
+        public LineSnapMode Mode
+        {
+            get => _mode;
+            set => Update(ref _mode, value);
+        }
+
+        public LineSnapTarget Target
+        {
+            get => _target;
+            set => Update(ref _target, value);
+        }
+
+        public double Threshold
+        {
+            get => _threshold;
+            set => Update(ref _threshold, value);
+        }
+
+        public ShapeStyle GuideStyle
+        {
+            get => _guideStyle;
+            set => Update(ref _guideStyle, value);
+        }
+    }
+
     public class LineSnapPointFilter : PointFilter
     {
         public override string Title => "Line-Snap";
