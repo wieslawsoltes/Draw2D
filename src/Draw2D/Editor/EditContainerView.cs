8ba5faf337da4c009132a9673106e08ad0b9e5b0
wieslawsoltes <wieslaw.soltes@gmail.com>
2019-05-18 08:03:23 +0000

    Move code

--- src/Draw2D/Editor/EditContainerView.cs
+++ src/Draw2D/Editor/EditContainerView.cs
@@ -21,59 +21,6 @@ using Draw2D.ViewModels.Tools;
 namespace Draw2D.Editor
 {
     [DataContract(IsReference = true)]
-    public class WindowSettings : Settings
-    {
-        private double _width;
-        private double _height;
-        private double _x;
-        private double _y;
-        private WindowState _windowState;
-
-        [DataMember(IsRequired = false, EmitDefaultValue = false)]
-        public double Width
-        {
-            get => _width;
-            set => Update(ref _width, value);
-        }
-
-        [DataMember(IsRequired = false, EmitDefaultValue = false)]
-        public double Height
-        {
-            get => _height;
-            set => Update(ref _height, value);
-        }
-
-        [DataMember(IsRequired = false, EmitDefaultValue = false)]
-        public double X
-        {
-            get => _x;
-            set => Update(ref _x, value);
-        }
-
-        [DataMember(IsRequired = false, EmitDefaultValue = false)]
-        public double Y
-        {
-            get => _y;
-            set => Update(ref _y, value);
-        }
-
-        [DataMember(IsRequired = false, EmitDefaultValue = false)]
-        public WindowState WindowState
-        {
-            get => _windowState;
-            set => Update(ref _windowState, value);
-        }
-
-        public override void Invalidate()
-        {
-            if (this.IsDirty)
-            {
-                this.IsDirty = false;
-            }
-        }
-    }
-
-    [DataContract(IsReference = true)]
     public class EditContainerView : ToolContext
     {
         private ISelection _selection;
