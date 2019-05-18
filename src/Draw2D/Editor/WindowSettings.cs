d468e504a05058d1002f7de9504ad519ac602e2b
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2019-05-18 17:22:00 +0200

    Update WindowSettings.cs

--- src/Draw2D/Editor/WindowSettings.cs
+++ src/Draw2D/Editor/WindowSettings.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 using System.Runtime.Serialization;
-using Avalonia;
 using Avalonia.Controls;
 using Draw2D.ViewModels;
 
