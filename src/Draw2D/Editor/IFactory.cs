ea738995551ca4de0d8fef5902c38c7d67aecbc4
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2019-05-22 00:36:28 +0200

    Create IFactory.cs

---
+++ src/Draw2D/Editor/IFactory.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using Draw2D.ViewModels.Containers;
+
+namespace Draw2D.Editor
+{
+    public interface IFactory
+    {
+        IToolContext CreateToolContext();
+        IContainerView CreateContainerView(string title);
+    }
+}
