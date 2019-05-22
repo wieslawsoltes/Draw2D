bf1a84ff2a2a016a38a2ab96744818579fc85211
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2019-05-22 17:58:46 +0200

    Update SkiaHelper.cs

--- src/Draw2D/Editor/Renderers/SkiaHelper.cs
+++ src/Draw2D/Editor/Renderers/SkiaHelper.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using Draw2D.ViewModels;
 using Draw2D.ViewModels.Shapes;
