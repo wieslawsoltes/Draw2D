6dd439d6eeec20d63388a8085ed9a63a737acdaa
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2019-05-23 18:21:03 +0200

    Update App.xaml.cs

--- src/Draw2D/App.xaml.cs
+++ src/Draw2D/App.xaml.cs
@@ -197,7 +197,7 @@ namespace Draw2D
         public static AppBuilder BuildAvaloniaApp()
             => AppBuilder.Configure<App>()
                          .UsePlatformDetect()
-                         .With(new Win32PlatformOptions { AllowEglInitialization = true })
+                         .With(new Win32PlatformOptions { AllowEglInitialization = false })
                          .With(new X11PlatformOptions { UseGpu = true, UseEGL = true })
                          .With(new AvaloniaNativePlatformOptions { UseGpu = true })
                          .UseSkia()
