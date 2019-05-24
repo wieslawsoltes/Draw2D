fdcbda768f76c1ad9977a1f8e830f5d4fed1a27e
wieslawsoltes <wieslaw.soltes@gmail.com>
2019-05-24 12:23:14 +0000

    Fix binding

--- src/Draw2D/Converters/StyleIdToStyleConverter.cs
+++ src/Draw2D/Converters/StyleIdToStyleConverter.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 using System;
+using System.Collections.Generic;
 using System.Globalization;
 using Avalonia;
 using Avalonia.Data.Converters;
@@ -10,22 +11,13 @@ using Draw2D.ViewModels.Style;
 
 namespace Draw2D.Converters
 {
-    public class StyleIdToStyleConverter : IValueConverter
+    public class StyleIdToStyleConverter : IMultiValueConverter
     {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        public object Convert(IList<object> values, Type targetType, object parameter, CultureInfo culture)
         {
-            if (value is string styleId && parameter is IStyleLibrary styleLibrary)
+            if (values?.Count == 2 && values[0] is string styleId && values[1] is IStyleLibrary styleLibrary)
             {
                 return styleLibrary.Get(styleId);
-            }
-            return AvaloniaProperty.UnsetValue;
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            if (value is ShapeStyle style)
-            {
-                return style.Title;
             }
             return AvaloniaProperty.UnsetValue;
         }
