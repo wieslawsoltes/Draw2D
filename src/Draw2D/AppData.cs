e071bd55d5deb52fae058fd69940759e0b01dd21
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2019-05-24 20:34:15 +0200

    Added AppData class

---
+++ src/Draw2D/AppData.cs
@@ -0,0 +1,236 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System.IO;
+using Avalonia;
+using Avalonia.Controls;
+using Draw2D.Editor;
+using Draw2D.ViewModels.Containers;
+using Draw2D.Views;
+
+namespace Draw2D
+{
+    public static class AppData
+    {
+        public static string StylesPath { get; set; }
+        public static string EditorPath { get; set; }
+        public static string WindowPath { get; set; }
+        public static IFactory Factory { get; set; }
+        public static IStyleLibrary StyleLibrary { get; set; }
+        public static IToolContext ToolContext { get; set; }
+        public static WindowSettings WindowSettings { get; set; }
+
+        static AppData()
+        {
+            StylesPath = "styles.json";
+            EditorPath = "editor.json";
+            WindowPath = "window.json";
+
+            CreateFactory();
+
+            if (Design.IsDesignMode)
+            {
+                CreateStyleLibrary();
+                CreateToolContext();
+                InitContainerViews();
+                CreateWindowSettings();
+            }
+        }
+
+        public static void CreateFactory()
+        {
+            Factory = new EditorFactory();
+        }
+
+        public static void CreateStyleLibrary()
+        {
+            StyleLibrary = Factory.CreateStyleLibrary();
+        }
+
+        public static void CreateToolContext()
+        {
+            ToolContext = Factory.CreateToolContext();
+            ToolContext.StyleLibrary = StyleLibrary;
+
+            if (ToolContext is EditorToolContext editorToolContext)
+            {
+                editorToolContext.Factory = Factory;
+                editorToolContext.NewContainerView("View");
+                editorToolContext.CurrentDirectory = Directory.GetCurrentDirectory();
+                editorToolContext.AddFiles(editorToolContext.CurrentDirectory);
+            }
+        }
+
+        public static void InitContainerViews()
+        {
+            if (ToolContext is EditorToolContext editorToolContext)
+            {
+                editorToolContext.Factory = Factory;
+                foreach (var containerView in editorToolContext.ContainerViews)
+                {
+                    editorToolContext.InitContainerView(containerView);
+                }
+            }
+        }
+
+        public static void CreateWindowSettings()
+        {
+            WindowSettings = new WindowSettings()
+            {
+                Width = 1320,
+                Height = 690,
+                X = double.NaN,
+                Y = double.NaN,
+                WindowState = WindowState.Normal
+            };
+        }
+
+        public static void SetWindowSettings(Window window)
+        {
+            if (!double.IsNaN(WindowSettings.Width))
+            {
+                window.Width = WindowSettings.Width;
+            }
+
+            if (!double.IsNaN(WindowSettings.Height))
+            {
+                window.Height = WindowSettings.Height;
+            }
+
+            if (!double.IsNaN(WindowSettings.X) && !double.IsNaN(WindowSettings.Y))
+            {
+                window.Position = new PixelPoint((int)WindowSettings.X, (int)WindowSettings.Y);
+                window.WindowStartupLocation = WindowStartupLocation.Manual;
+            }
+            else
+            {
+                window.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            }
+
+            window.WindowState = WindowSettings.WindowState;
+        }
+
+        public static void GetWindowSettings(Window window)
+        {
+            WindowSettings.Width = window.Width;
+            WindowSettings.Height = window.Height;
+            WindowSettings.X = window.Position.X;
+            WindowSettings.Y = window.Position.Y;
+            WindowSettings.WindowState = window.WindowState;
+        }
+
+        public static Window Open()
+        {
+            if (File.Exists(StylesPath))
+            {
+                StyleLibrary = JsonSerializer.FromJsonFile<IStyleLibrary>(StylesPath);
+            }
+            else
+            {
+                CreateStyleLibrary();
+            }
+
+            if (File.Exists(EditorPath))
+            {
+                ToolContext = JsonSerializer.FromJsonFile<IToolContext>(EditorPath);
+                ToolContext.StyleLibrary = StyleLibrary;
+                InitContainerViews();
+            }
+            else
+            {
+                CreateToolContext();
+            }
+
+            if (File.Exists(WindowPath))
+            {
+                WindowSettings = JsonSerializer.FromJsonFile<WindowSettings>(WindowPath);
+            }
+            else
+            {
+                CreateWindowSettings();
+            }
+
+            var window = new MainWindow
+            {
+                DataContext = ToolContext
+            };
+
+            SetWindowSettings(window);
+
+            window.Closing += (sender, e) =>
+            {
+                GetWindowSettings(window);
+            };
+            return window;
+        }
+
+        public static void Save()
+        {
+            JsonSerializer.ToJsonFile(StylesPath, ToolContext.StyleLibrary);
+            JsonSerializer.ToJsonFile(EditorPath, ToolContext);
+            JsonSerializer.ToJsonFile(WindowPath, WindowSettings);
+
+            WindowSettings = null;
+            ToolContext.Dispose();
+            ToolContext = null;
+            StyleLibrary = null;
+            Factory = null;
+        }
+
+        public static bool ParseArgs(string[] args)
+        {
+            if (args.Length == 1)
+            {
+                var command = args[0];
+
+                if (command == "--new-styles")
+                {
+                    var styleLibrary = Factory.CreateStyleLibrary();
+                    JsonSerializer.ToJsonFile("styles.json", styleLibrary);
+                    return false;
+                }
+
+                if (command == "--new-view")
+                {
+                    var containerView = Factory.CreateContainerView("View");
+                    JsonSerializer.ToJsonFile(containerView.Title + ".json", containerView);
+                    return false;
+                }
+
+                if (command == "--new-editor")
+                {
+                    var toolContext = Factory.CreateToolContext();
+                    JsonSerializer.ToJsonFile("editor.json", toolContext);
+                    return false;
+                }
+
+                if (command == "--demo")
+                {
+                    var toolContext = Factory.CreateToolContext();
+                    var styleLibrary = Factory.CreateStyleLibrary();
+                    toolContext.StyleLibrary = styleLibrary;
+                    if (toolContext is EditorToolContext editorToolContext)
+                    {
+                        editorToolContext.NewContainerView("Demo");
+                        editorToolContext.CreateDemoGroup(editorToolContext);
+                        JsonSerializer.ToJsonFile("Demo.json", editorToolContext.ContainerView);
+                    }
+                    return false;
+                }
+            }
+            else if (args.Length == 4)
+            {
+                var command = args[0];
+
+                if (command == "--export")
+                {
+                    var styleLibrary = JsonSerializer.FromJsonFile<IStyleLibrary>(args[1]);
+                    var containerView = JsonSerializer.FromJsonFile<ContainerView>(args[2]);
+                    EditorToolContext.Export(args[3], containerView, styleLibrary);
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ src/Draw2D/Program.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+using Avalonia;
+using Avalonia.Logging.Serilog;
+
+namespace Draw2D
+{
+    class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            if (AppData.ParseArgs(args) == true)
+            {
+                BuildAvaloniaApp().Start(AppMain, args);
+            }
+        }
+
+        public static AppBuilder BuildAvaloniaApp()
+            => AppBuilder.Configure<App>()
+                         .UsePlatformDetect()
+                         .With(new Win32PlatformOptions { AllowEglInitialization = false })
+                         .With(new X11PlatformOptions { UseGpu = true, UseEGL = true })
+                         .With(new AvaloniaNativePlatformOptions { UseGpu = true })
+                         .UseSkia()
+                         .LogToDebug();
+
+        private static void AppMain(Application app, string[] args)
+        {
+            var window = AppData.Open();
+            if (window != null)
+            {
+                app.Run(window);
+                AppData.Save();
+            }
+            else
+            {
+                app.Shutdown();
+            }
+        }
+    }
+}
