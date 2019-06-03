08bf1eb01ba88d3054c6ba7ce9b4bf9a5d601996
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2019-06-03 17:15:43 +0200

    Create Log.cs

---
+++ src/Draw2D/Log.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+
+namespace Draw2D
+{
+    public static class Log
+    {
+        public static void WriteLine(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
