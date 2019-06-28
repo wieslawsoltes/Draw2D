303ccb649f6f8fed9348d77d7c9786cbea0a4b9c
wieslawsoltes <wieslaw.soltes@gmail.com>
2019-06-28 10:39:10 +0000

    Add missing files

---
+++ src/Draw2D/ViewModels/Tools/Core/TwoPointsShapeType.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Draw2D.ViewModels.Tools
+{
+    public enum TwoPointsShapeType
+    {
+        Line = 0,
+        Rectangle = 1,
+        Ellipse = 2,
+        Text = 3
+    }
+}
