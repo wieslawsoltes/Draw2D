2e123670d2588e4489da151f724dddfb6d821c2a
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2019-07-01 17:11:14 +0200

    Update Library`1.cs

--- src/Draw2D/ViewModels/Containers/Core/Library`1.cs
+++ src/Draw2D/ViewModels/Containers/Core/Library`1.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Runtime.Serialization;
 
 namespace Draw2D.ViewModels.Containers
