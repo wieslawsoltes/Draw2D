e5d1dcceda4a3df38f79469bcc01a1df0c3a1b94
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2019-07-06 20:17:08 +0200

    Update AvaloniaXamlConverter.cs

--- src/Draw2D.Editor/Export/AvaloniaXamlConverter.cs
+++ src/Draw2D.Editor/Export/AvaloniaXamlConverter.cs
@@ -1,11 +1,10 @@
 ﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 using System;
-using System.Text;
 using System.Collections.Generic;
+using System.Text;
 using Draw2D.ViewModels;
 using Draw2D.ViewModels.Containers;
-using Draw2D.ViewModels.Shapes;
 using Draw2D.ViewModels.Tools;
 
 namespace Draw2D.Export
