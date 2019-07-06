0d96adead4455fca5b034c93fb10bce776650543
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2019-07-06 20:17:10 +0200

    Update SkiaCanvasConverter.cs

--- src/Draw2D.Editor/Export/SkiaCanvasConverter.cs
+++ src/Draw2D.Editor/Export/SkiaCanvasConverter.cs
@@ -1,17 +1,12 @@
 ﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 using System;
-using System.Text;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.IO;
-using System.Runtime.Serialization;
+using System.Text;
 using System.Xml;
 using System.Xml.Linq;
 using Draw2D.Presenters;
-using Draw2D.ViewModels;
 using Draw2D.ViewModels.Containers;
-using Draw2D.ViewModels.Shapes;
 using Draw2D.ViewModels.Tools;
 using SkiaSharp;
 
