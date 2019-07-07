3e5f70e668acfd17f95fa7c6f1df39e988ab1a0b
wieslawsoltes <wieslaw.soltes@gmail.com>
2019-07-07 08:54:51 +0000

    Refactored bounds types

---
+++ src/Draw2D.Skia/Renderers/Bounds/ChildNode.cs
@@ -0,0 +1,64 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+using System.Collections.Generic;
+using Draw2D.ViewModels;
+using Draw2D.ViewModels.Containers;
+using Draw2D.ViewModels.Shapes;
+using SkiaSharp;
+
+namespace Draw2D.Renderers
+{
+    public readonly struct ChildNode : IDisposable
+    {
+        public readonly IBaseShape Shape;
+        public readonly string StyleId;
+        public readonly double dX;
+        public readonly double dY;
+        public readonly double Scale;
+        public readonly SKPath Geometry;
+        public readonly SKRect Bounds;
+
+        public ChildNode(IBaseShape shape, string styleId, double dx, double dy, double scale, SKPath geometry)
+        {
+            this.Shape = shape;
+            this.StyleId = styleId;
+            this.dX = dx;
+            this.dY = dy;
+            this.Scale = scale;
+            this.Geometry = geometry;
+            geometry.GetBounds(out this.Bounds);
+        }
+
+        public bool Contains(float x, float y, ContainsMode mode)
+        {
+            if (mode == ContainsMode.Geometry)
+            {
+                return Geometry.Contains(x, y);
+            }
+            else if (mode == ContainsMode.Bounds)
+            {
+                return Bounds.Contains(x, y);
+            }
+            return false;
+        }
+
+        public bool Intersects(SKPath geometry)
+        {
+            using (var result = this.Geometry.Op(geometry, SKPathOp.Intersect))
+            {
+                return result?.IsEmpty == false;
+            }
+        }
+
+        public bool Intersects(ref SKRect rect)
+        {
+            return this.Bounds.IntersectsWith(rect);
+        }
+
+        public void Dispose()
+        {
+            Geometry?.Dispose();
+        }
+    }
+}
---
+++ src/Draw2D.Skia/Renderers/Bounds/RootNode.cs
@@ -0,0 +1,95 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+using System.Collections.Generic;
+using Draw2D.ViewModels;
+using Draw2D.ViewModels.Containers;
+using Draw2D.ViewModels.Shapes;
+using SkiaSharp;
+
+namespace Draw2D.Renderers
+{
+    public readonly struct RootNode : IDisposable
+    {
+        public readonly IBaseShape Shape;
+        public readonly IList<ChildNode> Children;
+
+        public RootNode(IBaseShape shape)
+        {
+            this.Shape = shape;
+            this.Children = new List<ChildNode>();
+        }
+
+        public bool Contains(float x, float y, ContainsMode mode, out IBaseShape rootShape, out IBaseShape childShape)
+        {
+            rootShape = null;
+            childShape = null;
+            for (int i = 0; i < Children.Count; i++)
+            {
+                if (mode == ContainsMode.Geometry)
+                {
+                    if (Children[i].Geometry.Contains(x, y))
+                    {
+                        rootShape = this.Shape;
+                        childShape = Children[i].Shape;
+                        return true;
+                    }
+                }
+                else if (mode == ContainsMode.Bounds)
+                {
+                    if (Children[i].Bounds.Contains(x, y))
+                    {
+                        rootShape = this.Shape;
+                        childShape = Children[i].Shape;
+                        return true;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return false;
+        }
+
+        public bool Intersects(SKPath geometry, out IBaseShape rootShape, out IBaseShape childShape)
+        {
+            rootShape = null;
+            childShape = null;
+            for (int i = 0; i < Children.Count; i++)
+            {
+                if (Children[i].Intersects(geometry))
+                {
+                    rootShape = this.Shape;
+                    childShape = Children[i].Shape;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool Intersects(ref SKRect rect, out IBaseShape rootShape, out IBaseShape childShape)
+        {
+            rootShape = null;
+            childShape = null;
+            for (int i = 0; i < Children.Count; i++)
+            {
+                if (Children[i].Bounds.IntersectsWith(rect))
+                {
+                    rootShape = this.Shape;
+                    childShape = Children[i].Shape;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void Dispose()
+        {
+            for (int i = 0; i < Children?.Count; i++)
+            {
+                Children[i].Dispose();
+            }
+        }
+    }
+}
