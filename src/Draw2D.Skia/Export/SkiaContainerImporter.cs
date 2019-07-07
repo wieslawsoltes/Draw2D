e8d328d623f4820e247fc26011295597e6d21a7d
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2019-07-07 17:18:51 +0200

    Update SkiaContainerImporter.cs

--- src/Draw2D.Skia/Export/SkiaContainerImporter.cs
+++ src/Draw2D.Skia/Export/SkiaContainerImporter.cs
@@ -2,13 +2,8 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 using System;
 using System.IO;
-using System.Text;
-using System.Xml;
-using System.Xml.Linq;
-using Draw2D.Presenters;
 using Draw2D.ViewModels.Containers;
 using Draw2D.ViewModels.Tools;
-using SkiaSharp;
 
 namespace Draw2D.Export
 {
@@ -16,11 +11,11 @@ namespace Draw2D.Export
     {
         internal static void ImportSvg(IToolContext context, string path, IContainerView containerView)
         {
-            var svg = new SkiaSharp.Extended.Svg.SKSvg();
-            using (var stream = File.Open(path, FileMode.Open))
+            var xml = File.ReadAllText(path);
+            if (!string.IsNullOrEmpty(xml))
             {
-                var picture = svg.Load(stream);
-                //var image = SKImage.FromPicture(picture, picture.CullRect.Size.ToSizeI());
+                var picture = SkiaHelper.ToSKPicture(xml);
+                var image = SkiaHelper.ToSKImage(picture);
             }
         }
 
