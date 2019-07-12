6f3dbe0a3b1f42e8ba4d89bbd100ac2c3ddaf947
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2019-07-12 20:47:14 +0200

    Update IPathEffectFactory.cs

--- src/Draw2D.ViewModels/Style/Core/IPathEffectFactory.cs
+++ src/Draw2D.ViewModels/Style/Core/IPathEffectFactory.cs
@@ -1,9 +1,27 @@
 ﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
 
 namespace Draw2D.ViewModels.Style
 {
     public interface IPathEffectFactory
     {
+        void Create1DPathTranslateEffect(Action<IPathEffect> setter);
+        void Create1DPathRotateEffect(Action<IPathEffect> setter);
+        void Create1DPathMorphEffect(Action<IPathEffect> setter);
+        void Create2DLineHatchHorizontalLinesEffect(Action<IPathEffect> setter);
+        void Create2DLineHatchVerticalLinesEffect(Action<IPathEffect> setter);
+        void Create2DLineHatchDiagonalLinesEffect(Action<IPathEffect> setter);
+        void Create2DPathTileEffect(Action<IPathEffect> setter);
+        void CreateCornerEffect(Action<IPathEffect> setter);
+        void CreateDashEffect(Action<IPathEffect> setter);
+        void CreateDotEffect(Action<IPathEffect> setter);
+        void CreateDashDotEffect(Action<IPathEffect> setter);
+        void CreateDashDotDotEffect(Action<IPathEffect> setter);
+        void CreateDiscreteEffect(Action<IPathEffect> setter);
+        void CreateTrimEffect(Action<IPathEffect> setter);
+        void CreateComposeEffect(Action<IPathEffect> setter);
+        void CreateSumEffect(Action<IPathEffect> setter);
+        void ResetPathEffect(Action<IPathEffect> setter);
     }
 }
