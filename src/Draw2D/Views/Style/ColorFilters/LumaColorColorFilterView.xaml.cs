0f5ebac738a89d899a6e4534027bc1ac1304d6ca
wieslawsoltes <wieslaw.soltes@gmail.com>
2019-07-16 07:55:31 +0000

    Added color filter view stubs

---
+++ src/Draw2D/Views/Style/ColorFilters/LumaColorColorFilterView.xaml.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Draw2D.Views.Style.ColorFilters
+{
+    public class LumaColorColorFilterView : UserControl
+    {
+        public LumaColorColorFilterView()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
