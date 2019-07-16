9aae5a561d0783daae6df7a111329df985932bf9
wieslawsoltes <wieslaw.soltes@gmail.com>
2019-07-16 06:27:55 +0000

    Added ColorShader properties

--- src/Draw2D.ViewModels/Style/Shaders/ColorShader.cs
+++ src/Draw2D.ViewModels/Style/Shaders/ColorShader.cs
@@ -8,17 +8,40 @@ namespace Draw2D.ViewModels.Style.Shaders
     [DataContract(IsReference = true)]
     public class ColorShader : ViewModelBase, IShader
     {
-        // TODO:
+        private ArgbColor _color;
+
+        [DataMember(IsRequired = false, EmitDefaultValue = false)]
+        public ArgbColor Color
+        {
+            get => _color;
+            set => Update(ref _color, value);
+        }
 
         public ColorShader()
         {
+        }
+
+        public ColorShader(ArgbColor color)
+        {
+            this.Color = color;
+        }
+
+        public ColorShader(byte a, byte r, byte g, byte b)
+        {
+            this.Color = new ArgbColor(a, r, b, b);
+        }
+
+        public static IShader MakeColor()
+        {
+            return new ColorShader(new ArgbColor(255, 0, 0, 0));
         }
 
         public object Copy(Dictionary<object, object> shared)
         {
             return new ColorShader()
             {
-                Title = this.Title
+                Title = this.Title,
+                Color = (ArgbColor)this.Color.Copy(shared)
             };
         }
     }
