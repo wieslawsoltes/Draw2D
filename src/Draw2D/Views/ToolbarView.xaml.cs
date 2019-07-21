0ce02d99828494f815a131c1bf41c4dc26f681f5
wieslawsoltes <wieslaw.soltes@gmail.com>
2019-07-21 10:29:42 +0000

    Fixed icons

--- src/Draw2D/Views/ToolbarView.xaml.cs
+++ src/Draw2D/Views/ToolbarView.xaml.cs
@@ -5,9 +5,9 @@ using Avalonia.Markup.Xaml;
 
 namespace Draw2D.Views
 {
-    public class ToolbarViev : UserControl
+    public class ToolbarView : UserControl
     {
-        public ToolbarViev()
+        public ToolbarView()
         {
             InitializeComponent();
         }
