33e635ab5f38a6bc59159342c413a7cb2812118a
wieslawsoltes <wieslaw.soltes@gmail.com>
2019-07-22 11:35:53 +0000

    Refactored

--- SvgDemo/Program.cs
+++ SvgDemo/Program.cs
@@ -9,110 +9,120 @@ namespace SvgDemo
 {
     class Program
     {
-        static void Print(SvgDocument doc)
+        static void ResetColor()
         {
-            Console.ForegroundColor = ConsoleColor.Blue;
-            Console.WriteLine($"{doc}");
-            Print(doc, "    ");
             Console.ResetColor();
         }
 
-        static void Print(SvgElement element, string indent = "", string indentAttribute = "    ")
+        static void WriteLine(string value, ConsoleColor color = ConsoleColor.Black)
         {
-            // Children
+            Console.ForegroundColor = color;
+            Console.WriteLine(value);
+        }
 
-            if (element.Children.Count > 0)
+        static void Print(SvgDocument doc)
+        {
+            WriteLine($"{doc}", ConsoleColor.Blue);
+            Print(doc, "    ");
+            ResetColor();
+        }
+
+        static void PrintAttributes(SvgElement element, string indent = "", string indentAttribute = "")
+        {
+            if (!string.IsNullOrEmpty(element.ID))
             {
-                Console.ForegroundColor = ConsoleColor.Gray;
-                Console.WriteLine($"{indent}<Children>");
+                WriteLine($"{indent}{indentAttribute}[id]={element.ID}", ConsoleColor.Black);
             }
 
-            foreach (var child in element.Children)
+            // Attributes
+
+            if (element.Fill != null && element.Fill != SvgPaintServer.None)
             {
-                Console.ForegroundColor = ConsoleColor.Blue;
-                Console.WriteLine($"{indent}{child}, [id]={child.ID}");
-
-                // Attributes
-
-                if (child.Fill != null && child.Fill != SvgPaintServer.None)
-                {
-                    Console.ForegroundColor = ConsoleColor.Black;
-                    Console.WriteLine($"{indent}{indentAttribute}[fill]={child.Fill.GetType()}");
-                }
-
-                if (child.Stroke != null && child.Stroke != SvgPaintServer.None)
-                {
-                    Console.ForegroundColor = ConsoleColor.Black;
-                    Console.WriteLine($"{indent}{indentAttribute}[stroke]={child.Stroke.GetType()}");
-                }
-
-                if (child.FillRule != SvgFillRule.NonZero)
-                {
-                    Console.ForegroundColor = ConsoleColor.Black;
-                    Console.WriteLine($"{indent}{indentAttribute}[fill-rule]={child.FillRule}");
-                }
-
-                if (child.FillOpacity != 1f)
-                {
-                    Console.ForegroundColor = ConsoleColor.Black;
-                    Console.WriteLine($"{indent}{indentAttribute}[fill-opacity]={child.FillOpacity}");
-                }
-
-                if (child.StrokeWidth != 1f)
-                {
-                    Console.ForegroundColor = ConsoleColor.Black;
-                    Console.WriteLine($"{indent}{indentAttribute}[stroke-width]={child.StrokeWidth}");
-                }
-
-                // ...
-
-                Print(child, indent + "    ");
+                WriteLine($"{indent}{indentAttribute}[fill]={element.Fill.GetType()}", ConsoleColor.Black);
             }
+
+            if (element.Stroke != null && element.Stroke != SvgPaintServer.None)
+            {
+                WriteLine($"{indent}{indentAttribute}[stroke]={element.Stroke.GetType()}", ConsoleColor.Black);
+            }
+
+            if (element.FillRule != SvgFillRule.NonZero)
+            {
+                WriteLine($"{indent}{indentAttribute}[fill-rule]={element.FillRule}", ConsoleColor.Black);
+            }
+
+            if (element.FillOpacity != 1f)
+            {
+                WriteLine($"{indent}{indentAttribute}[fill-opacity]={element.FillOpacity}", ConsoleColor.Black);
+            }
+
+            if (element.StrokeWidth != 1f)
+            {
+                WriteLine($"{indent}{indentAttribute}[stroke-width]={element.StrokeWidth}", ConsoleColor.Black);
+            }
+
+            // ...
+
 
             // CustomAttributes
 
             if (element.CustomAttributes.Count > 0)
             {
-                Console.ForegroundColor = ConsoleColor.Gray;
-                Console.WriteLine($"{indent}<CustomAttributes>");
-            }
-
-            foreach (var attribute in element.CustomAttributes)
-            {
-                Console.ForegroundColor = ConsoleColor.Black;
-                Console.WriteLine($"{indent}[{attribute.Key}]={attribute.Value}");
+                WriteLine($"{indent}{indentAttribute}<CustomAttributes>", ConsoleColor.Gray);
+                
+                foreach (var attribute in element.CustomAttributes)
+                {
+                    WriteLine($"{indent}{indentAttribute}[{attribute.Key}]={attribute.Value}", ConsoleColor.Black);
+                }
             }
 
             // Transforms
 
             if (element.Transforms.Count > 0)
             {
-                Console.ForegroundColor = ConsoleColor.Gray;
-                Console.WriteLine($"{indent}<Transforms>");
-            }
+                WriteLine($"{indent}{indentAttribute}<Transforms>", ConsoleColor.Gray);
+                WriteLine($"{indent}{indentAttribute}[transform]", ConsoleColor.Black);
 
-            foreach (var transform in element.Transforms)
+                foreach (var transform in element.Transforms)
+                {
+                    WriteLine($"{indent}{indentAttribute}{transform}", ConsoleColor.Black);
+                }
+            }
+        }
+
+        static void Print(SvgElement element, string indent = "", string indentAttribute = "")
+        {
+            // Attributes
+
+            PrintAttributes(element, indent, indentAttribute);
+
+            // Children
+
+            if (element.Children.Count > 0)
             {
-                Console.ForegroundColor = ConsoleColor.Blue;
-                Console.WriteLine($"{indent}{transform}");
+                WriteLine($"{indent}<Children>", ConsoleColor.Gray);
+
+                foreach (var child in element.Children)
+                {
+                    WriteLine($"{indent}{child}", ConsoleColor.Blue);
+                    Print(child, indent + "    ", indentAttribute);
+                }
             }
 
             // Nodes
 
             if (element.Nodes.Count > 0)
             {
-                Console.ForegroundColor = ConsoleColor.Gray;
-                Console.WriteLine($"{indent}<Nodes>");
-            }
+                WriteLine($"{indent}<Nodes>", ConsoleColor.Gray);
 
-            foreach (var node in element.Nodes)
-            {
-                Console.ForegroundColor = ConsoleColor.Black;
-                Console.WriteLine($"{indent}{node.Content}");
+                foreach (var node in element.Nodes)
+                {
+                    WriteLine($"{indent}{node.Content}", ConsoleColor.Black);
+                }
             }
         }
 
-        static void Main(string[] args)
+        static void Run(string[] args)
         {
             if (args.Length < 1)
             {
@@ -121,24 +131,33 @@ namespace SvgDemo
 
             for (int i = 0; i < args.Length; i++)
             {
-                try
-                {
-                    string path = args[i];
-                    Console.WriteLine($"Path: {path}");
+                string path = args[i];
+                WriteLine($"Path: {path}", ConsoleColor.Black);
 
-                    var doc = SvgDocument.Open<SvgDocument>(path, null);
-                    Print(doc);
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                    Console.WriteLine(ex.StackTrace);
-                    if (ex.InnerException != null)
-                    {
-                        Console.WriteLine(ex.InnerException);
-                        Console.WriteLine(ex.InnerException.StackTrace);
-                    }
-                }
+                var doc = SvgDocument.Open<SvgDocument>(path, null);
+                Print(doc);
+            }
+        }
+
+        static void Error(Exception ex)
+        {
+            WriteLine($"{ex.Message}", ConsoleColor.Red);
+            WriteLine($"{ex.StackTrace}", ConsoleColor.Black);
+            if (ex.InnerException != null)
+            {
+                Error(ex.InnerException);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Run(args);
+            }
+            catch (Exception ex)
+            {
+                Error(ex);
             }
         }
     }
