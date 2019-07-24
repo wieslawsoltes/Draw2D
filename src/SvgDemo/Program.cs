d5cfba2999ef19e3e7c639113000bae49ce6662d
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2019-07-24 20:45:27 +0200

    Update Program.cs

--- src/SvgDemo/Program.cs
+++ src/SvgDemo/Program.cs
@@ -40,6 +40,151 @@ namespace SvgDemo
         }
     }
 
+    public static class SkiaSvgRenderer
+    {
+        public static SKColor GetColor(SvgColourServer svgColourServer, float opacity, bool forStroke = false)
+        {
+            if (svgColourServer == SvgPaintServer.None)
+            {
+                return SKColors.Transparent;
+            }
+
+            if (svgColourServer == SvgColourServer.NotSet && forStroke)
+            {
+                return SKColors.Transparent;
+            }
+
+            var colour = svgColourServer.Colour;
+            byte alpha = (byte)Math.Round((opacity * (svgColourServer.Colour.A / 255.0)) * 255);
+
+            return new SKColor(colour.R, colour.G, colour.B, alpha);
+        }
+
+        public static SKPath ToSKPath(SvgPath svgPath)
+        {
+            var sb = new StringBuilder();
+
+            foreach (var svgSegment in svgPath.PathData)
+            {
+                sb.AppendLine(svgSegment.ToString());
+            }
+
+            var pathData = sb.ToString();
+
+            return SKPath.ParseSvgPathData(pathData);
+        }
+
+        public static void Draw(SKCanvas canvas, Element element)
+        {
+            switch (element.Original)
+            {
+                case SvgFragment svgFragment:
+                    {
+                    }
+                    break;
+                case SvgLine svgLine:
+                    {
+                        var p0 = new SKPoint((float)svgLine.StartX, (float)svgLine.StartY);
+                        var p1 = new SKPoint((float)svgLine.EndX, (float)svgLine.EndY);
+
+                        if (svgLine.Stroke != null && svgLine.Stroke != SvgColourServer.NotSet)
+                        {
+                            if (svgLine.Stroke is SvgColourServer svgColourServer)
+                            {
+                                var paint = new SKPaint();
+
+                                paint.Color = GetColor(svgColourServer, svgLine.StrokeOpacity, true);
+                                paint.StrokeWidth = svgLine.StrokeWidth;
+                                paint.Style = SKPaintStyle.Stroke;
+
+                                canvas.DrawLine(p0, p1, paint);
+                            }
+                        }
+                    }
+                    break;
+                case SvgPath svgPath:
+                    {
+                        var path = ToSKPath(svgPath);
+                        if (path == null || path.IsEmpty)
+                        {
+                            break;
+                        }
+
+                        if (svgPath.Fill != null && svgPath.Fill != SvgColourServer.NotSet)
+                        {
+                            var paint = new SKPaint();
+
+                            if (svgPath.Fill is SvgColourServer svgColourServer)
+                            {
+                                paint.Color = paint.Color = GetColor(svgColourServer, svgPath.StrokeOpacity, false);
+                                paint.Style = SKPaintStyle.Fill;
+                            }
+
+                            canvas.DrawPath(path, paint);
+                        }
+
+                        if (svgPath.Stroke != null && svgPath.Stroke != SvgColourServer.NotSet)
+                        {
+                            var paint = new SKPaint();
+
+                            if (svgPath.Stroke is SvgColourServer svgColourServer)
+                            {
+                                paint.Color = paint.Color = GetColor(svgColourServer, svgPath.StrokeOpacity, true);
+                                paint.StrokeWidth = svgPath.StrokeWidth;
+                                paint.Style = SKPaintStyle.Stroke;
+                            }
+
+                            canvas.DrawPath(path, paint);
+                        }
+                    }
+                    break;
+            }
+
+            Draw(canvas, element.Children);
+        }
+
+        public static void Draw(SKCanvas canvas, List<Element> elements)
+        {
+            foreach (var element in elements)
+            {
+                Draw(canvas, element);
+            }
+        }
+
+        public static void SaveImage(string path, Element element, SKEncodedImageFormat format = SKEncodedImageFormat.Png, int quality = 100)
+        {
+            if (element.Original is SvgDocument svgDocument)
+            {
+                float width = svgDocument.Width;
+                float Height = svgDocument.Height;
+
+                var info = new SKImageInfo((int)width, (int)Height);
+                using (var bitmap = new SKBitmap(info))
+                {
+                    using (var canvas = new SKCanvas(bitmap))
+                    {
+                        canvas.Clear(SKColors.Transparent);
+
+                        Draw(canvas, element.Children);
+
+                        using (var image = SKImage.FromBitmap(bitmap))
+                        using (var data = image.Encode(format, quality))
+                        {
+                            if (data != null)
+                            {
+                                using (var stream = File.OpenWrite(path))
+                                {
+                                    data.SaveTo(stream);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+
     public class SvgDebug
     {
         public StringBuilder Builder { get; set; }
@@ -1511,139 +1656,6 @@ namespace SvgDemo
             return element;
         }
 
-        private SKColor GetColor(SvgColourServer svgColourServer, float opacity, bool forStroke = false)
-        {
-            if (svgColourServer == SvgPaintServer.None)
-            {
-                return SKColors.Transparent;
-            }
-
-            if (svgColourServer == SvgColourServer.NotSet && forStroke)
-            {
-                return SKColors.Transparent;
-            }
-
-            var colour = svgColourServer.Colour;
-            byte alpha = (byte)Math.Round((opacity * (svgColourServer.Colour.A / 255.0)) * 255);
-
-            return new SKColor(colour.R, colour.G, colour.B, alpha);
-        }
-
-        private void Draw(SKCanvas canvas, Element element)
-        {
-            switch (element.Original)
-            {
-                case SvgLine svgLine:
-                    {
-                        var paint = new SKPaint();
-
-                        var p0 = new SKPoint((float)svgLine.StartX, (float)svgLine.StartY);
-                        var p1 = new SKPoint((float)svgLine.EndX, (float)svgLine.EndY);
-
-                        if (svgLine.Stroke != null && svgLine.Stroke != SvgColourServer.NotSet)
-                        {
-                            if (svgLine.Stroke is SvgColourServer svgColourServer)
-                            {
-                                paint.Color = GetColor(svgColourServer, svgLine.StrokeOpacity, true);
-                                paint.StrokeWidth = svgLine.StrokeWidth;
-                                paint.Style = SKPaintStyle.Stroke;
-                            }
-                        }
-
-                        canvas.DrawLine(p0, p1, paint);
-                    }
-                    break;
-                case SvgPath svgPath:
-                    {
-                        var sb = new StringBuilder();
-
-                        foreach (var svgSegment in svgPath.PathData)
-                        {
-                            sb.AppendLine(svgSegment.ToString());
-                        }
-
-                        var pathData = sb.ToString();
-
-                        var path = SKPath.ParseSvgPathData(pathData);
-
-                        if (path == null || path.IsEmpty)
-                        {
-                            break;
-                        }
-
-                        if (svgPath.Fill != null && svgPath.Fill != SvgColourServer.NotSet)
-                        {
-                            var paint = new SKPaint();
-
-                            if (svgPath.Fill is SvgColourServer svgColourServer)
-                            {
-                                paint.Color = paint.Color = GetColor(svgColourServer, svgPath.StrokeOpacity, false);
-                                paint.Style = SKPaintStyle.Fill;
-                            }
-
-                            canvas.DrawPath(path, paint);
-                        }
-
-                        if (svgPath.Stroke != null && svgPath.Stroke != SvgColourServer.NotSet)
-                        {
-                            var paint = new SKPaint();
-
-                            if (svgPath.Stroke is SvgColourServer svgColourServer)
-                            {
-                                paint.Color = paint.Color = GetColor(svgColourServer, svgPath.StrokeOpacity, true);
-                                paint.StrokeWidth = svgPath.StrokeWidth;
-                                paint.Style = SKPaintStyle.Stroke;
-                            }
-
-                            canvas.DrawPath(path, paint);
-                        }
-                    }
-                    break;
-            }
-
-            Draw(canvas, element.Children);
-        }
-
-        private void Draw(SKCanvas canvas, List<Element> elements)
-        {
-            foreach (var element in elements)
-            {
-                Draw(canvas, element);
-            }
-        }
-
-        private void SaveAsPng(string path, Element element)
-        {
-            if (element.Original is SvgDocument svgDocument)
-            {
-                float width = svgDocument.Width;
-                float Height = svgDocument.Height;
-
-                var info = new SKImageInfo((int)width, (int)Height);
-                using (var bitmap = new SKBitmap(info))
-                {
-                    using (var canvas = new SKCanvas(bitmap))
-                    {
-                        canvas.Clear(SKColors.Transparent);
-
-                        Draw(canvas, element.Children);
-
-                        using (var image = SKImage.FromBitmap(bitmap))
-                        using (var data = image.Encode(SKEncodedImageFormat.Png, 100))
-                        {
-                            if (data != null)
-                            {
-                                using (var stream = File.OpenWrite(path))
-                                {
-                                    data.SaveTo(stream);
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
         public void Run(string path)
         {
             WriteLine($"# {path}");
@@ -1661,7 +1673,7 @@ namespace SvgDemo
                         File.WriteAllText(path + ".yml", yaml);
                     }
 
-                    SaveAsPng(path + ".png", document);
+                    SkiaSvgRenderer.SaveImage(path + ".png", document, SKEncodedImageFormat.Png, 100);
                 }
             }
         }
