0035cfdf0d89e177044cf43515f3fa83aa0b37f2
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2019-07-24 22:48:45 +0200

    Update Element.cs

--- src/SvgDemo/Element.cs
+++ src/SvgDemo/Element.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using Svg;
 
 namespace SvgDemo
@@ -9,23 +8,19 @@ namespace SvgDemo
         public SvgElement Original { get; set; }
         public Element Parent { get; set; }
         public string Name { get; set; }
-        public Type Type { get; set; }
-        public Dictionary<string, object> Attributes { get; set; }
         public List<Element> Children { get; set; }
 
         public Element()
         {
-            Attributes = new Dictionary<string, object>();
             Children = new List<Element>();
         }
 
-        public Element(SvgElement original, Element parent, string name, Type type)
+        public Element(SvgElement original, Element parent, string name)
             : this()
         {
             Original = original;
             Parent = parent;
             Name = name;
-            Type = type;
         }
     }
 }
