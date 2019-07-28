e781c650c73b8cd039466278e41fd8808d199d23
wieslawsoltes <wieslaw.soltes@gmail.com>
2019-07-28 09:22:18 +0000

    Updated command-line

--- svg/Svg.Skia.Converter/Program.cs
+++ svg/Svg.Skia.Converter/Program.cs
@@ -7,6 +7,7 @@ using System.CommandLine.Invocation;
 using System.Diagnostics;
 using System.IO;
 using System.Text;
+using System.Threading.Tasks;
 using SkiaSharp;
 using Svg;
 
@@ -24,138 +25,150 @@ namespace Svg.Skia.Converter
             }
         }
 
-        static void Usage()
+        static async Task<int> Main(string[] args)
         {
-            Console.WriteLine($"Usage: Svg.Skia.Converter -f path [-o path]");
-            Console.WriteLine($"       Svg.Skia.Converter -d path [-o path]");
-        }
-
-        static void Main(string[] args)
-        {
-            try
+            var optionFile = new Option(new [] { "--file", "-f" }, "The relative or absolute path to the input file")
             {
-                if (args.Length != 2 && args.Length != 4)
-                {
-                    Usage();
-                    return;
-                }
+                Argument = new Argument<string>(defaultValue: () => null)
+            };
 
-                var paths = new List<string>();
-                var output = default(string);
+            var optionDirectory = new Option(new [] { "--directory", "-d" }, "The relative or absolute path to the input directory")
+            {
+                Argument = new Argument<string>(defaultValue: () => null)
+            };
 
-                if (args.Length == 4)
+            var optionOutput = new Option(new [] { "--output", "-o" }, "The relative or absolute path to the output directory")
+            {
+                Argument = new Argument<string>(defaultValue: () => null)
+            };
+
+            var optionPattern = new Option(new [] { "--pattern", "-p" }, "The search string to match against the names of files in the input path.")
+            {
+                Argument = new Argument<string>(defaultValue: () => "*.svg")
+            };
+
+            var optionScaleX = new Option(new [] { "--scaleX", "-sx" }, "The output image horizontal scaling factor")
+            {
+                Argument = new Argument<float>(defaultValue: () => 1f)
+            };
+
+            var optionScaleY = new Option(new [] { "--scaleY", "-sY" }, "The output image vertical scaling factor")
+            {
+                Argument = new Argument<float>(defaultValue: () => 1f)
+            };
+
+            var rootCommand = new RootCommand();
+
+            rootCommand.AddOption(optionFile);
+            rootCommand.AddOption(optionDirectory);
+            rootCommand.AddOption(optionOutput);
+            rootCommand.AddOption(optionPattern);
+            rootCommand.AddOption(optionScaleX);
+            rootCommand.AddOption(optionScaleY);
+
+            rootCommand.Handler = CommandHandler.Create<string, string, string, string, float, float>((file, directory, output, pattern, scaleX, scaleY) =>
+            {
+                try
                 {
-                    if (args[2] == "-o")
+                    var paths = new List<string>();
+
+                    if (file != null)
                     {
-                        output = args[3];
+                        paths.Add(file);
                     }
-                    else
-                    {
-                        Usage();
-                        return; 
-                    }
-                }
 
-                switch (args[0])
-                {
-                    case "-f":
+                    if (directory != null)
+                    {
+                        var files = Directory.EnumerateFiles(directory, pattern);
+                        if (files != null)
                         {
-                            paths.Add(args[1]);
+                            paths.AddRange(files);
                         }
-                        break;
-                        case "-d":
+                    }
+
+                    if (!string.IsNullOrEmpty(output))
+                    {
+                        if (!Directory.Exists(output))
                         {
-                            var files = Directory.EnumerateFiles(args[1], "*.svg");
-                            if (files != null)
+                            Directory.CreateDirectory(output);
+                        }    
+                    }
+
+                    var sw = Stopwatch.StartNew();
+
+                    int success = 0;
+
+                    foreach (var path in paths)
+                    {
+                        try
+                        {
+                            Console.ForegroundColor = ConsoleColor.Black;
+                            Console.WriteLine($"File: {path}");
+
+                            var svg = new Svg();
+                            var picture = svg.Load(path);
+                            if (picture != null)
                             {
-                                paths.AddRange(files);
-                            }
-                        }
-                        break;
-                    default:
-                        {
-                            Usage();
-                            return;
-                        }
-                }
-
-                var sw = Stopwatch.StartNew();
-
-                int success = 0;
-                int errors = 0;
-
-                if (!string.IsNullOrEmpty(output))
-                {
-                    if (!Directory.Exists(output))
-                    {
-                        Directory.CreateDirectory(output);
-                    }    
-                }
-
-                foreach (var path in paths)
-                {
-                    try
-                    {
-                        Console.ForegroundColor = ConsoleColor.DarkGray;
-                        Console.WriteLine($"File: {path}");
-
-                        var svg = new Svg();
-                        var picture = svg.Load(path);
-                        if (picture != null)
-                        {
-                            var extension = Path.GetExtension(path);
+                                var extension = Path.GetExtension(path);
 #if false
-                        var svgDebug = new SvgDebug()
-                        {
-                            Builder = new StringBuilder(),
-                            IndentTab = "  ",
-                            PrintSvgElementAttributesEnabled = true,
-                            PrintSvgElementCustomAttributesEnabled = true,
-                            PrintSvgElementChildrenEnabled = true,
-                            PrintSvgElementNodesEnabled = false
-                        };
-                        svgDebug.PrintSvgElement(svgDocument, "", "");
-                        if (svgDebug.Builder != null)
-                        {
-                            var yaml = svgDebug.Builder.ToString();
-                            string ymlPath = path.Remove(path.Length - extension.Length) + ".yml";
-                            if (!string.IsNullOrEmpty(output))
+                            var svgDebug = new SvgDebug()
                             {
-                                ymlPath = Path.Combine(output, Path.GetFileName(ymlPath));
+                                Builder = new StringBuilder(),
+                                IndentTab = "  ",
+                                PrintSvgElementAttributesEnabled = true,
+                                PrintSvgElementCustomAttributesEnabled = true,
+                                PrintSvgElementChildrenEnabled = true,
+                                PrintSvgElementNodesEnabled = false
+                            };
+                            svgDebug.PrintSvgElement(svgDocument, "", "");
+                            if (svgDebug.Builder != null)
+                            {
+                                var yaml = svgDebug.Builder.ToString();
+                                string ymlPath = path.Remove(path.Length - extension.Length) + ".yml";
+                                if (!string.IsNullOrEmpty(output))
+                                {
+                                    ymlPath = Path.Combine(output, Path.GetFileName(ymlPath));
+                                }
+                                File.WriteAllText(ymlPath, yaml);
                             }
-                            File.WriteAllText(ymlPath, yaml);
-                        }
 #endif
-                            string pngPath = path.Remove(path.Length - extension.Length) + ".png";
-                            if (!string.IsNullOrEmpty(output))
-                            {
-                                pngPath = Path.Combine(output, Path.GetFileName(pngPath));
-                            }
-                            svg.Save(pngPath, SKEncodedImageFormat.Png, 100, 1, 1);
-                        }
+                                string pngPath = path.Remove(path.Length - extension.Length) + ".png";
+                                if (!string.IsNullOrEmpty(output))
+                                {
+                                    pngPath = Path.Combine(output, Path.GetFileName(pngPath));
+                                }
 
-                        Console.ForegroundColor = ConsoleColor.Green;
-                        Console.WriteLine($"Succes: {path}");
-                        success++;
+                                svg.Save(pngPath, SKEncodedImageFormat.Png, 100, scaleX, scaleY);
+                            }
+
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine($"Succes: {path}");
+                            success++;
+                        }
+                        catch (Exception)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine($"Error: {path}");
+                        }
                     }
-                    catch (Exception)
+
+                    sw.Stop();
+
+                    if (paths.Count > 0)
                     {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine($"Error: {path}");
-                        errors++;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine($"Done: {sw.Elapsed} ({success}/{paths.Count})");
+                        Console.ResetColor();
                     }
                 }
+                catch (Exception ex)
+                {
+                    Console.ResetColor();
+                    Error(ex);
+                }
+            });
 
-                sw.Stop();
-                Console.ForegroundColor = ConsoleColor.DarkGray;
-                Console.WriteLine($"Done: {sw.Elapsed} ({success}/{paths.Count})");
-                Console.ResetColor();
-            }
-            catch (Exception ex)
-            {
-                Console.ResetColor();
-                Error(ex);
-            }
+            return await rootCommand.InvokeAsync(args);
         }
     }
 }
