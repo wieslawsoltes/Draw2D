5d427dd576e3b17045b43a93bd84840431e6787f
wieslawsoltes <wieslaw.soltes@gmail.com>
2019-07-28 10:48:35 +0000

    Updates

--- svg/Svg.Skia.Converter/Program.cs
+++ svg/Svg.Skia.Converter/Program.cs
@@ -178,7 +178,7 @@ namespace Svg.Skia.Converter
 
             var rootCommand = new RootCommand()
             {
-                Description = "Converts an svg file to an encoded image."
+                Description = "Converts a svg file to an encoded image."
             };
 
             rootCommand.AddOption(optionFile);
