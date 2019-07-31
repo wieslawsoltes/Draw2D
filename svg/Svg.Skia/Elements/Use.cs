7e1d10bfaf59e70a37fdab270420803dfaf73008
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2019-08-01 01:29:20 +0200

    Moved code

---
+++ svg/Svg.Skia/Elements/Use.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//
+// Parts of this source file are adapted from the https://github.com/vvvv/SVG
+using SkiaSharp;
+using Svg;
+
+namespace Svg.Skia
+{
+    internal struct Use
+    {
+        public SKMatrix matrix;
+
+        public Use(SvgUse svgUse)
+        {
+            matrix = SvgHelper.GetSKMatrix(svgUse.Transforms);
+        }
+    }
+}
