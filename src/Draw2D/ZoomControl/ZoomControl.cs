d4627468df2b45456818b31375c52fd0fef7da8e
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-04-05 11:31:49 +0200

    Update ZoomControl.cs

--- src/Draw2D/ZoomControl/ZoomControl.cs
+++ src/Draw2D/ZoomControl/ZoomControl.cs
@@ -506,7 +506,6 @@ namespace Draw2D.ZoomControl
             }
         }
 
-
         protected override void OnPointerWheelChanged(PointerWheelEventArgs e)
         {
             base.OnPointerWheelChanged(e);
