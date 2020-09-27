ea10fde756a8eb62bfbf8c3c24d978c54c65a491
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-09-27 14:10:39 +0200

    Remove

--- src/Draw2D/Views/MenuView.xaml.cs
+++ src/Draw2D/Views/MenuView.xaml.cs
@@ -1,30 +1,10 @@
-﻿using Avalonia;
-using Avalonia.Controls;
-using Avalonia.Interactivity;
+﻿using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 
 namespace Draw2D.Views
 {
     public class MenuView : UserControl
     {
-        public static readonly StyledProperty<bool> DrawDirtyRectsProperty =
-            AvaloniaProperty.Register<MenuView, bool>(nameof(DrawDirtyRects));
-
-        public bool DrawDirtyRects
-        {
-            get { return GetValue(DrawDirtyRectsProperty); }
-            set { SetValue(DrawDirtyRectsProperty, value); }
-        }
-
-        public static readonly StyledProperty<bool> DrawFpsProperty =
-            AvaloniaProperty.Register<MenuView, bool>(nameof(DrawFps));
-
-        public bool DrawFps
-        {
-            get { return GetValue(DrawFpsProperty); }
-            set { SetValue(DrawFpsProperty, value); }
-        }
-
         public MenuView()
         {
             InitializeComponent();
@@ -33,25 +13,6 @@ namespace Draw2D.Views
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
-        }
-
-        protected override void OnAttachedToVisualTree(VisualTreeAttachmentEventArgs e)
-        {
-            base.OnAttachedToVisualTree(e);
-            DrawDirtyRects = VisualRoot.Renderer.DrawDirtyRects;
-            DrawFps = VisualRoot.Renderer.DrawFps;
-        }
-
-        public void DebugDrawDirtyRects_Click(object sender, RoutedEventArgs e)
-        {
-            VisualRoot.Renderer.DrawDirtyRects = !VisualRoot.Renderer.DrawDirtyRects;
-            DrawDirtyRects = VisualRoot.Renderer.DrawDirtyRects;
-        }
-
-        public void DebugDrawFps_Click(object sender, RoutedEventArgs e)
-        {
-            VisualRoot.Renderer.DrawFps = !VisualRoot.Renderer.DrawFps;
-            DrawFps = VisualRoot.Renderer.DrawFps;
         }
     }
 }
