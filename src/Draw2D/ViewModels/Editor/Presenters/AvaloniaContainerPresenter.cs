898ba4fef71d1b653a227037251634b8d88da8b7
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-11-08 23:06:16 +0100

    Update AvaloniaContainerPresenter.cs

--- src/Draw2D/ViewModels/Editor/Presenters/AvaloniaContainerPresenter.cs
+++ src/Draw2D/ViewModels/Editor/Presenters/AvaloniaContainerPresenter.cs
@@ -3,6 +3,7 @@ using Avalonia;
 using Avalonia.Media;
 using Avalonia.Media.Imaging;
 using Avalonia.Platform;
+using Avalonia.Rendering.SceneGraph;
 using Avalonia.Skia;
 using Draw2D.ViewModels.Containers;
 using Draw2D.ViewModels.Tools;
@@ -14,7 +15,6 @@ namespace Draw2D.Presenters
     {
         private readonly IToolContext _context;
         private readonly IContainerView _view;
-        private RenderTargetBitmap _renderTarget;
         private double _renderScaling;
         private IContainerPresenter _editorContainerPresenter;
 
@@ -22,19 +22,12 @@ namespace Draw2D.Presenters
         {
             _context = context;
             _view = view;
-            _renderTarget = null;
             _renderScaling = 1.0;
             _editorContainerPresenter = new SkiaEditorContainerPresenter(_context, _view);
         }
 
         public void Dispose()
         {
-            if (_renderTarget != null)
-            {
-                _renderTarget.Dispose();
-                _renderTarget = null;
-            }
-
             if (_editorContainerPresenter != null)
             {
                 _editorContainerPresenter.Dispose();
@@ -42,49 +35,54 @@ namespace Draw2D.Presenters
             }
         }
 
+        private class CustomDrawOp : ICustomDrawOperation
+        {
+            private readonly Action<SKCanvas> _draw;
+
+            public CustomDrawOp(Action<SKCanvas> draw, Rect bounds)
+            {
+                _draw = draw;
+                Bounds = bounds;
+            }
+            
+            public void Dispose()
+            {
+            }
+
+            public bool HitTest(Point p)
+            {
+                return false;
+            }
+
+            public void Render(IDrawingContextImpl context)
+            {
+                using var lease = context.GetFeature<ISkiaSharpApiLeaseFeature>()?.Lease();
+                if (lease?.SkCanvas is { } skCanvas)
+                {
+                    _draw.Invoke(skCanvas);
+                }
+            }
+
+            public Rect Bounds { get; }
+
+            public bool Equals(ICustomDrawOperation other)
+            {
+                return false;
+            }
+        }
+
         public void Draw(object context, double width, double height, double dx, double dy, double zx, double zy, double renderScaling)
         {
             if (context is DrawingContext drawingContext && width > 0 && height > 0)
             {
-                if (_renderTarget == null)
+                void DrawLocal(SKCanvas skCanvas)
                 {
-                    _renderScaling = renderScaling;
-                    _renderTarget = new RenderTargetBitmap(
-                        new PixelSize((int)(width / _renderScaling), (int)(height / _renderScaling)), 
-                        new Vector(96, 96 ));
-                }
-                else if (_renderTarget.PixelSize.Width != (int)width || _renderTarget.PixelSize.Height != (int)height || Math.Abs(_renderScaling - renderScaling) > double.Epsilon)
-                {
-                    _renderScaling = renderScaling;
-                    _renderTarget.Dispose();
-                    _renderTarget = new RenderTargetBitmap(
-                        new PixelSize((int)(width / _renderScaling), (int)(height / _renderScaling)), 
-                        new Vector(96, 96));
-                }
-
-                using var drawingContextImpl = _renderTarget.CreateDrawingContext(null);
-                var leaseFeature = drawingContextImpl.GetFeature<ISkiaSharpApiLeaseFeature>();
-                if (leaseFeature is null)
-                {
-                    return;
-                }
-                using var lease = leaseFeature.Lease();
-
-                if (lease?.SkCanvas is { } skCanvas)
-                {
-                    var skMatrix = SKMatrix.CreateScale((float)(1.0 / renderScaling), (float)(1.0 / renderScaling));
-
                     skCanvas.Save();
-                    skCanvas.SetMatrix(skMatrix);
- 
                     _editorContainerPresenter.Draw(skCanvas, width, height, dx, dy, zx, zy, renderScaling);
-
                     skCanvas.Restore();
                 }
 
-                drawingContext.DrawImage(_renderTarget,
-                    new Rect(0, 0, _renderTarget.PixelSize.Width, _renderTarget.PixelSize.Height),
-                    new Rect(0, 0, _renderTarget.PixelSize.Width, _renderTarget.PixelSize.Height));
+                drawingContext.Custom(new CustomDrawOp(DrawLocal, new Rect(0d, 0d, width, height)));
             }
         }
     }
