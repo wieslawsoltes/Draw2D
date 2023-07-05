475386243747e6c349e046692927101feca61b9f
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-07-05 21:46:09 +0200

    Update to 11.0.0

--- src/Draw2D/Program.cs
+++ src/Draw2D/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using Avalonia;
-using Avalonia.OpenGL;
-using Avalonia.Win32;
 
 namespace Draw2D;
 
@@ -23,50 +20,9 @@ internal class Program
         return 0;
     }
 
-    private static AngleOptions GetAngleOptions()
-    {
-        return new AngleOptions()
-        {
-            AllowedPlatformApis = new List<AngleOptions.PlatformApi>
-            {
-                AngleOptions.PlatformApi.DirectX11
-            }
-        };
-    }
-
-    private static Win32PlatformOptions GetWin32PlatformOptions()
-    {
-        return new Win32PlatformOptions
-        {
-            AllowEglInitialization = true
-        };
-    }
-
-    private static X11PlatformOptions GetX11PlatformOptions()
-    {
-        return new X11PlatformOptions
-        {
-            EnableMultiTouch = true,
-            UseGpu = true,
-            UseEGL = true
-        };
-    }
-
-    private static AvaloniaNativePlatformOptions GetAvaloniaNativePlatformOptions()
-    {
-        return new AvaloniaNativePlatformOptions
-        {
-            UseGpu = true,
-        };
-    }
-
     public static AppBuilder BuildAvaloniaApp()
         => AppBuilder.Configure<App>()
             .UsePlatformDetect()
-            .With(GetWin32PlatformOptions())
-            .With(GetAngleOptions())
-            .With(GetX11PlatformOptions())
-            .With(GetAvaloniaNativePlatformOptions())
             .UseSkia()
             .LogToTrace();
 }
