9743b3760957db0466801192e65d05eda6eb84f6
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2023-09-29 21:25:05 +0200

    Fix commands

--- src/Draw2D/ViewModels/Style/Core/IPathEffectFactory.cs
+++ src/Draw2D/ViewModels/Style/Core/IPathEffectFactory.cs
@@ -1,9 +1,29 @@
 ﻿using System;
+using CommunityToolkit.Mvvm.Input;
 
 namespace Draw2D.ViewModels.Style;
 
 public interface IPathEffectFactory
 {
+    IRelayCommand<Action<IPathEffect>> Create1DPathTranslateEffectCommand { get; }
+    IRelayCommand<Action<IPathEffect>> Create1DPathRotateEffectCommand { get; }
+    IRelayCommand<Action<IPathEffect>> Create1DPathMorphEffectCommand { get; }
+    IRelayCommand<Action<IPathEffect>> Create2DLineHatchHorizontalLinesEffectCommand { get; }
+    IRelayCommand<Action<IPathEffect>> Create2DLineHatchVerticalLinesEffectCommand { get; }
+    IRelayCommand<Action<IPathEffect>> Create2DLineHatchDiagonalLinesEffectCommand { get; }
+    IRelayCommand<Action<IPathEffect>> Create2DPathTileEffectCommand { get; }
+    IRelayCommand<Action<IPathEffect>> CreateCornerEffectCommand { get; }
+    IRelayCommand<Action<IPathEffect>> CreateDashEffectCommand { get; }
+    IRelayCommand<Action<IPathEffect>> CreateDotEffectCommand { get; }
+    IRelayCommand<Action<IPathEffect>> CreateDashDotEffectCommand { get; }
+    IRelayCommand<Action<IPathEffect>> CreateDashDotDotEffectCommand { get; }
+    IRelayCommand<Action<IPathEffect>> CreateDiscreteEffectCommand { get; }
+    IRelayCommand<Action<IPathEffect>> CreateTrimEffectCommand { get; }
+    IRelayCommand<Action<IPathEffect>> CreateComposeEffectCommand { get; }
+    IRelayCommand<Action<IPathEffect>> CreateSumEffectCommand { get; }
+    IRelayCommand<IPathEffect> CopyCommand { get; }
+    IRelayCommand<Action<IPathEffect>> PasteCommand { get; }
+    IRelayCommand<Action<IPathEffect>> DeleteCommand { get; }
     void Create1DPathTranslateEffect(Action<IPathEffect> setter);
     void Create1DPathRotateEffect(Action<IPathEffect> setter);
     void Create1DPathMorphEffect(Action<IPathEffect> setter);
--- src/Draw2D/ViewModels/Style/Paints/PathEffectFactory.cs
+++ src/Draw2D/ViewModels/Style/Paints/PathEffectFactory.cs
@@ -1,94 +1,112 @@
 ﻿using System;
+using CommunityToolkit.Mvvm.Input;
 using Draw2D.ViewModels.Style.PathEffects;
 
 namespace Draw2D.ViewModels.Style;
 
-public class PathEffectFactory : IPathEffectFactory
+public partial class PathEffectFactory : IPathEffectFactory
 {
     public static IPathEffectFactory Instance { get; } = new PathEffectFactory();
 
     private IPathEffect _copy = null;
 
+    [RelayCommand]
     public void Create1DPathTranslateEffect(Action<IPathEffect> setter)
     {
         setter?.Invoke(Path1DPathEffect.MakeTranslate());
     }
 
+    [RelayCommand]
     public void Create1DPathRotateEffect(Action<IPathEffect> setter)
     {
         setter?.Invoke(Path1DPathEffect.MakeRotate());
     }
 
+    [RelayCommand]
     public void Create1DPathMorphEffect(Action<IPathEffect> setter)
     {
         setter?.Invoke(Path1DPathEffect.MakeMorph());
     }
 
+    [RelayCommand]
     public void Create2DLineHatchHorizontalLinesEffect(Action<IPathEffect> setter)
     {
         setter?.Invoke(Path2DLineEffect.MakeHatchHorizontalLines());
     }
 
+    [RelayCommand]
     public void Create2DLineHatchVerticalLinesEffect(Action<IPathEffect> setter)
     {
         setter?.Invoke(Path2DLineEffect.MakeHatchVerticalLines());
     }
 
+    [RelayCommand]
     public void Create2DLineHatchDiagonalLinesEffect(Action<IPathEffect> setter)
     {
         setter?.Invoke(Path2DLineEffect.MakeHatchDiagonalLines());
     }
 
+    [RelayCommand]
     public void Create2DPathTileEffect(Action<IPathEffect> setter)
     {
         setter?.Invoke(Path2DPathEffect.MakeTile());
     }
 
+    [RelayCommand]
     public void CreateCornerEffect(Action<IPathEffect> setter)
     {
         setter?.Invoke(PathCornerEffect.MakeCorner());
     }
 
+    [RelayCommand]
     public void CreateDashEffect(Action<IPathEffect> setter)
     {
         setter?.Invoke(PathDashEffect.MakeDash());
     }
 
+    [RelayCommand]
     public void CreateDotEffect(Action<IPathEffect> setter)
     {
         setter?.Invoke(PathDashEffect.MakeDot());
     }
 
+    [RelayCommand]
     public void CreateDashDotEffect(Action<IPathEffect> setter)
     {
         setter?.Invoke(PathDashEffect.MakeDashDot());
     }
 
+    [RelayCommand]
     public void CreateDashDotDotEffect(Action<IPathEffect> setter)
     {
         setter?.Invoke(PathDashEffect.MakeDashDotDot());
     }
 
+    [RelayCommand]
     public void CreateDiscreteEffect(Action<IPathEffect> setter)
     {
         setter?.Invoke(PathDiscreteEffect.MakeDiscrete());
     }
 
+    [RelayCommand]
     public void CreateTrimEffect(Action<IPathEffect> setter)
     {
         setter?.Invoke(PathTrimEffect.MakeTrim());
     }
 
+    [RelayCommand]
     public void CreateComposeEffect(Action<IPathEffect> setter)
     {
         setter?.Invoke(PathComposeEffect.MakeCompose());
     }
 
+    [RelayCommand]
     public void CreateSumEffect(Action<IPathEffect> setter)
     {
         setter?.Invoke(PathSumEffect.MakeSum());
     }
 
+    [RelayCommand]
     public void Copy(IPathEffect pathEffect)
     {
         if (pathEffect != null)
@@ -97,6 +115,7 @@ public class PathEffectFactory : IPathEffectFactory
         }
     }
 
+    [RelayCommand]
     public void Paste(Action<IPathEffect> setter)
     {
         if (_copy != null)
@@ -105,6 +124,7 @@ public class PathEffectFactory : IPathEffectFactory
         }
     }
 
+    [RelayCommand]
     public void Delete(Action<IPathEffect> setter)
     {
         setter?.Invoke(null);
